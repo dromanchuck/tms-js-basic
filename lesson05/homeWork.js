b4bf0f8505848d127670b3b0763e5c929145c767
MichaelZavalo <mihailzavalo@yandex.ru>
2020-07-07 16:47:42 +0300

    homework5 ex0,1

--- lesson05/homeWork.js
+++ lesson05/homeWork.js
@@ -1,3 +1,4 @@
+'use strict'
 /**
  * Домашнее задание к занятию 5
  * Каждому нужно создать новую ветку от ветки master. Название ветки должно содержать имя + первая буква фамилии + /lesson05.
@@ -19,6 +20,36 @@
  * Напишите код, который уберет эти дубликаты из созданного массива.
  */
 
+// first solution
+let ex0Arr = [1,2,3,4,5,6,2,8,9,5,11,2,13,3,5];
+let uniq = [];
+ex0Arr.forEach(function(outerItem, outerIndex) {
+    ex0Arr.forEach(function(innerItem, innerIndex) {
+        if(outerIndex !== innerIndex && outerItem === innerItem) {
+            innerItem = 'dublicate';
+        }
+    });
+    if (outerItem !== "dublicate") {
+    uniq.push(outerItem);
+    };
+});
+console.log(uniq);
+
+//second solution
+let ex0Arr = [1,2,3,4,5,6,2,8,9,5,11,2,13,3,5];
+let uniq = ex0Arr.filter((element,index) => index === ex0Arr.includes(element));
+console.log(uniq);
+
+//third solution
+let ex0Arr = [1,2,3,4,5,6,2,8,9,5,11,2,13,3,5];
+let uniq = [];
+ex0Arr.forEach(element => {
+    if (!uniq.includes(element)) {
+        uniq.push(element);
+    }
+});
+console.log(uniq);
+
 /**
  * Задание 1
  * Создайте 2 массива с разной длинной.
@@ -26,12 +57,35 @@
  * соответствующих элементов заданных массивов.
  */
 
+//first solution
+let ex1ArrOne = [1,2,3],
+    ex1ArrTwo = [32,42,12,53,342,2];
+let result1Arr = ex1ArrTwo.map((item, index) => {
+    if(!isNaN(ex1ArrTwo[index])) return item;
+    return item + ex1ArrOne[index];    
+});
+console.log(result1Arr);
+
+//second solution
+let ex1ArrOne = [1,2,3],
+    ex1ArrTwo = [32,42,12,53,342,2];
+let result1Arr = ex1ArrTwo.reduce((acc, curr, i) => acc.concat(ex1ArrOne[i] + curr || curr), []);
+console.log(result1Arr);
+
 /**
  * Задание 2
  * Напишите код, который подсчитает количество повторяющихся элементов в массиве.
  * И вывести это количество в консоль.
  */
+/*
+let ex2Arr = [1,2,3,4,5,6,2,8,9,5,11,2,13,33,15];
 
+let sumOfElements = ex2Arr.reduce(function(){
+
+});
+
+console.log(sumOfElements);
+*/
 /**
  * Задание 3
  * Напишите код, который добавит символ двоеточие(':') между нечетными числами.
@@ -42,7 +96,9 @@
  * Задание 4
  * Создайте массив из 5 чисел и найдите наибольшее число и выведите его в консоль.
  */
+/*let ex4Arr = [1,2,3,4,5];
 
+*/
 /**
  * Задание 5
  * Переписать сортировку позырьком, используюя методы итерирования по массиву.
@@ -61,3 +117,9 @@
  * Необходимо написать код,который создаёт массив элементов представляющих собой разность
  * соответствующих элементов заданных массивов.
  */
+
+let ex7ArrOne = [1,2,3],
+    ex7ArrTwo = [32,42,12,53,342,2],
+    lengthArr = ex1ArrOne.length > ex1ArrTwo.length ? ex1ArrOne.length : ex1ArrTwo.length;
+
+let resultArr = ;
