418835290ef3575506122b51e14429f19a0a318d
Your Name <you@example.com>
2020-07-08 15:36:02 +0300

    homework_five

--- lesson04/homeWork.js
+++ lesson04/homeWork.js
@@ -15,144 +15,120 @@
  *
  */
 
-let array = [1, 2, 3, 4, 5];
-let resultArray = array.slice();
-let resultArray1 = [];
+//first
+let arr = [true, 65, 'Mission', 0.44];
 
-console.log(resultArray);
+let newArr = arr.map(person => person);
+console.log(newArr);
 
-for (let i = 0; i <= array.length - 1; i++) {
-  resultArray1.push(array[i]);
-}
+//second
+let arr = [true, 65, 'Mission', 0.44];
 
-console.log(resultArray1);
-
+let newArr = arr.filter(person => true);
+arr[1] = 40;
+console.log(newArr, arr);
 /**
  * Задание 1
  * Напишите код, который преобразовывает и объединяет все элементы массива в одно строковое значение.
  * Элементы массива будут разделены запятой. Получите результат двумя разными способами.
  */
-
-let array = [1, 2, 3, 4, 5];
-
-let str = array.join();
-let str1 = String(array);
-let str2 = "";
-
-for (let i = 0; i <= array.length - 1; i++) {
-  if (i === array.length - 1) {
-    str2 += array[i];
-  } else {
-    str2 += array[i] + ",";
-  }
-}
-
-console.log(str2);
-
+let arrTaskOne = ['high', 'parameter', 35, false],
+    firstResult;
+firstResult = arrTaskOne.reduce((total,element,index) => (index === arrTaskOne.length-1) ? total += element : (total += element + ', '),'');
+console.log(firstResult);
 /**
  * Задание 2
  * Необходимо создать массив из 15 элементов. В массиве обязательно должны быть одинаковые значения.
  * Напишите код, который уберет эти дубликаты из созданного массива.
  */
 
-let arr = [1, 2, 3, 4, 3, 5, 6, 7, 8, 9, 21, 1, 1, 0, 14];
-let uniq = [];
-console.log(arr);
-for (let i = 0; i <= arr.length - 1; i++) {
-  //arr[i]
 
-  for (let j = arr.length - 1; j >= 0; j--) {
-    if (i !== j && arr[i] === arr[j]) {
-      arr[j] = "dublicate";
-    }
-  }
+//secondDesicion
+let arrFirst = [1,1,242,"Mam",424,1,true,4,"Mam",242,44,24,44,true,242];
 
-  if (arr[i] !== "dublicate") {
-    uniq.push(arr[i]);
-  }
-}
+//first
+let arrWithoutDup = arrFirst.filter((element, index) => index === arrFirst.indexOf(element));
+console.log(arrWithoutDup);
 
-console.log(uniq);
-
+//second
+let arrWithoutDupRep = arrFirst.reduce((total, element) => {
+   return total.includes(element) ? total : [...total, element];
+},[])
+console.log(arrWithoutDupRep);
 /**
+ * 
  * Задание 3
  * Создайте 2 массива с разной длинной.
  * Необходимо написать код,который создаёт массив элементов представляющих собой сумму
  * соответствующих элементов заданных массивов.
  */
 
-let arr1 = [12, 2, 3];
-let arr2 = [1, 2, 3, 4, 5];
-let result = [];
-
-let currentArr = arr1.length > arr2.length ? arr1 : arr2;
-
-for (let i = 0; i <= currentArr.length - 1; i++) {
-  let a = Boolean(arr1[i]) ? arr1[i] : typeof arr1[i] === "string" ? "" : 0;
-  let b = Boolean(arr2[i]) ? arr2[i] : typeof arr1[i] === "string" ? "" : 0;
-  result[i] = a + b;
-}
-
-console.log(result);
-
+let arrOne = [31, "string", true, 767, 20],
+    arrTwo = ["string", 242, 42, 0];
+let arr3 = arrOne.map((value, index) => { 
+  if (arrTwo[index] === undefined && typeof value === 'string'){
+    arrTwo[index] = '';
+  } else if (arrTwo[index] === undefined && typeof value === 'number'){
+    arrTwo[index] = 0;
+  }
+  return value + arrTwo[index]});
+console.log(arr3);
 /**
  * Задание 4
  * Напишите код, который подсчитает количество повторяющихся элементов в массиве.
  * И вывести это количество в консоль.
  */
+let arrFirst = [1,1,242,"Mam",1,1,true,4,"Mam",424,44,24,44,true,242],
+    arrWithoutDup,
+    copy = 0;
+  arrWithoutDup = arrFirst
+  .reduce((total,element) => {
+  if (!total[element]){
+    total[element] = 1;
+  } else {
+    total[element] = total[element] + 1;
+  }
+  return total;
+  },{})
 
-let arr = [1, 2, 3, 4, 5, 4, 2, 1, 1];
-let result = {};
-let resultCount = 0;
-
-for (let i = 0; i <= arr.length - 1; i++) {
-  let count = 0;
-
-  for (let j = arr.length - 1; j >= 0; j--) {
-    if (i !== j && arr[i] === arr[j]) {
-      count++;
+  for (key in arrWithoutDup){
+    if (arrWithoutDup[key] > 1){
+      copy++;
     }
   }
-
-  result[arr[i]] = count;
-}
-
-for (let key in result) {
-  if (result[key] > 0) {
-    resultCount++;
-  }
-}
-
-console.log(resultCount);
-
+  console.log(copy);
 /**
  * Задание 5
  * Напишите код, который разворачивает исходный массив и сохраняет это в новую переменную.
  * Например: исходный массив - [1, 2, 3], результирующий массив - [3, 2, 1]
  */
 
-let array = [1, 2, 3, 4, 5, 6];
+let arrayNum = [1, 2, 3];
+let newArrayNum = arrayNum.reverse().map((element) =>{
+  return element;
+})
+console.log(newArrayNum);
 
-let resultArray = array.slice().reverse();
-
-console.log(array);
-
+const myArray = ["a", "b", "c", "d", "e"];
+const res = myArray.reduceRight((arr, element, index) => {
+    console.log(element, index); // cчетчик элементов c обратной стороны
+    return arr = [...arr, element]; //or (arr = arr.concat(element))
+}, []);
+console.log(res);
 /**
  * Задание 6
  * Напишите код, который проверит является строка полиндромом(слово, которое с обеих сторон читается одинаково,
  * например РЕПЕР, ШАЛАШ)
  */
 
-//string.toUpperCase(); 'test' -> 'TEST'
-//string.toLowerCase(); 'TeSt' -> 'test'
-
-let str = prompt("Polindrom?", "test");
-let arr = str.split("");
-let reversedArr = arr.slice().reverse();
-let reversedStr = reversedArr.join("");
-
-if (str.toLowerCase() === reversedStr.toLowerCase()) {
-  alert("Polindrom is detected");
+let str = "Шалаш",
+    createArr = str.split(''); // разделить на символы и создаст массив
+let reversedArr = createArr.reduceRight((total,element) => {
+  return total = (total = total.concat(element));
+},'');
+console.log(reversedArr);
+if (str.toLowerCase() === reversedArr.toLowerCase()){
+  console.log('Полиндром detected');
 } else {
-  alert("Polindrom is not detected");
+  console.log('Полиндром is not detected');
 }
--- lesson05/homeWork.js
+++ lesson05/homeWork.js
@@ -18,46 +18,121 @@
  * Необходимо создать массив из 15 элементов. В массиве обязательно должны быть одинаковые значения.
  * Напишите код, который уберет эти дубликаты из созданного массива.
  */
-
+let arrFirst = [1,1,242,"Mam",424,1,true,4,"Mam",242,44,24,44,true,242];
+let arrWithoutDup = arrFirst.filter((element, index) => index === arrFirst.indexOf(element));
+console.log(arrWithoutDup);
 /**
  * Задание 1
  * Создайте 2 массива с разной длинной.
  * Необходимо написать код,который создаёт массив элементов представляющих собой сумму
  * соответствующих элементов заданных массивов.
  */
-
+let arrOne = [31, "string", true, 767, 20],
+    arrTwo = ["string", 242, 42, 20];
+let arr3 = arrOne.reduce((acum, value, index) => {
+    return acum = [...acum,(value + arrTwo[index] || value)]
+},[]);
+console.log(arr3);
 /**
  * Задание 2
  * Напишите код, который подсчитает количество повторяющихся элементов в массиве.
  * И вывести это количество в консоль.
  */
+let arrFirst = [1,1,242,"Mam",1,1,true,4,"Mam",424,44,24,44,true,242],
+    arrWithoutDup,
+    copy = 0;
+  arrWithoutDup = arrFirst
+  .reduce((total,element) => {
+  if (!total[element]){
+    total[element] = 1;
+  } else {
+    total[element] = total[element] + 1;
+  }
+  return total;
+  },{})
 
+  for (key in arrWithoutDup){
+    if (arrWithoutDup[key] > 1){
+      copy++;
+    }
+  }
+  console.log(copy);
 /**
  * Задание 3
  * Напишите код, который добавит символ двоеточие(':') между нечетными числами.
  * Например, число 556 результат должен быть '5:56', 566 -> 566, 655 -> 65:5
  */
-
+let arrayNumb = [1,1,2,2,3,3,4,4,5];
+let arrayNumbNew = arrayNumb.reduce((acum,elem,index,array) => {
+ if (array[index]%2 === 0 && array[index+1]%2 === 0){
+   acum += array[index] + "->";
+ } else if (array[index]%2 > 0 && array[index+1]%2 > 0){
+   acum += array[index] + ":"; 
+ } else {acum += array[index] + ' '}
+ return acum;
+},'');
+console.log(arrayNumbNew);
 /**
  * Задание 4
  * Создайте массив из 5 чисел и найдите наибольшее число и выведите его в консоль.
  */
-
+let arrFirst = [2000,20,1123,150,0];
+let  arrWithoutDup = arrFirst
+.reduce((total,element) => {
+ if (element > total){
+ total = 0;
+ total += element;
+ }
+return total;
+},0);
+console.log(arrWithoutDup);
 /**
  * Задание 5
  * Переписать сортировку позырьком, используюя методы итерирования по массиву.
  * код сортировки находится по пути lesson04/index.js
  */
-
+let arr = [41,42,2424,12,2];
+let arrDup = arr
+.map((element,index,arrayElem) => arrayElem)
+.reduce((acum, element, index, arrayElem) => {
+  element.forEach((item, i) => {
+    if (item > element[i + 1]) {
+      let temp = item;
+      element[i] = element[i + 1];
+      element[i + 1] = temp;
+    }
+  });
+  acum += element + ' ';
+  return acum;
+},[]);
+console.log(arrDup);
 /**
  * Задание 6
  * Написать код, который заменит регистр каждого символа на противоположны.
  * Например 'Hello world' -> 'hELLO WORLD'
  */
-
+let arrStr = "Hello world";
+arrStr = [...arrStr];
+let newArrStr = [];
+ arrStr.forEach((elem, index, array) => {
+ if (array[index] === array[index].toLowerCase()){
+  array[index] = array[index].toUpperCase();
+ }
+ else if (array[index] === array[index].toUpperCase()){
+  array[index] = array[index].toLowerCase();
+ }
+ newArrStr.push(array[index]);
+})
+console.log(newArrStr.join(''));
 /**
  * Задание 7
  * Создайте 2 массива с разной длинной.
  * Необходимо написать код,который создаёт массив элементов представляющих собой разность
  * соответствующих элементов заданных массивов.
  */
+let arrOne = [31, 41, 1344, 767, 20,4234],
+    arrTwo = [5235, 242, 5242, 24, 144];
+let arr3 = arrOne.reduce((acum, value, index) => {
+    return acum = [...acum,(value - arrTwo[index] || value)]
+},[]);
+console.log(arr3);
