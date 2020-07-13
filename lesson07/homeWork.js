1d6070a70ff3e704da5755b8997614c9b67d554d
Your Name <you@example.com>
2020-07-13 16:13:07 +0300

    lesson07

--- lesson07/homeWork.js
+++ lesson07/homeWork.js
@@ -3,35 +3,101 @@
  * Написать функцию, которая вычислит минимальное значени всех аргументов функции и вернет это значение.
  * (Math.min использовать нельзя :-) )
  */
-
+function min(...args) {
+ let counter;
+ args.forEach((elem, i) => {
+  if (args[i] < args[i + 1]) {
+   counter = args[i];
+   args[i + 1] = counter;
+  } else {
+   counter = args[i];
+  }
+ });
+ return console.log(counter);
+}
+min(423, -23, 43, 1, 55, -24);
 /**
  * Задание 1
  * Написать функцию, которая  подсчитает сумму всех аргументов функции и вернет это значение.
  */
-
+function sum(...args) {
+ let newArg = args.reduce((counter, elem, i) => {
+  counter += elem;
+  return counter;
+ }, 0);
+ return newArg;
+}
+console.log(sum(1, 2, 34));
 /**
  * Задание 2
  * Написать функцию, которая будет возвращать количество вызовов этой функции в квадрате.
  * (Использовать замыкание)
  *
  */
-
+function sqrtCall() {
+ let counter = 0;
+ return function () {
+  counter++;
+  return console.log(counter ** counter);
+ };
+}
+let count = sqrtCall();
+count();
+count();
+count();
 /**
  * Задание 3
  * Написать функцию сравнения двух массивов, которая возвращает true или false в зависимости от того,
  * одинаковые у них элементы или нет.
  */
-
+function compare(arr1, arr2) {
+ return (
+  arr1.length === arr2.length && arr1.every((elem, i) => elem === arr2[i])
+ );
+}
+console.log(compare([3, 5], [3, 5]));
 /**
  * Задание 4
  * Написать функцию, которая принимает первым аргументом условие,
  * вторым и третьим аргментом функции(функции могут делать все что угодно на вам выбор).
  * Внутри если условие равно true, то выполнить первую функцию, если false, то выполнить вторую функцию.
  */
+let obj1 = {
+ company: "RStyle",
+ phone: 80298654242,
+ contact: 'Eugenii',
+ phoneAvail: "Недоступен"
+};
 
+function func1() {
+ obj1.phone = 80242242445;
+ obj1.contact = 'Pete';
+ obj1.phoneAvail = 'Доступен';
+ return console.log(obj1);
+}
+
+function func2() {
+ return console.log(obj1);
+}
+
+function trueFunction(cond, func1, func2) {
+ if (cond === true) {
+  return func1();
+ } else {
+  return func2();
+ }
+}
+
+trueFunction(obj1.phoneAvail === "Недоступен", func1, func2);
 /**
  * Задание 5
  * Сделать функцию, которая вычисляет разность между двумя числами и вернет разность.
  * Выполнить используя замыкание, результат должен выглядеть примерно так
  * let sub = substract(a)(b) // a - b
  */
+function difference(a) {
+ return function (b) {
+  return console.log(a - b);
+ };
+}
+difference(-1)(-12);
