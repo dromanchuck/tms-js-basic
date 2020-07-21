3d5c496f62c71013f3650d8704a040e0b3a37a2b
Andrey-Sushkevich <nagibator2346@gmail.com>
2020-07-21 18:02:44 +0300

    nometask9

--- lesson09/index.js
+++ lesson09/index.js
@@ -1,6 +1,16 @@
 let container = document.createElement("div");
 let header = document.createElement("h1");
 
+document.body.style.background = "white";
+setTimeout(() => document.body.style.background = "red", 5000);
+setTimeout(() => document.body.style.background = "white", 10000);
+/*
+второй вариант
+document.body.classList=("body");
+setInterval(() => {
+  document.body.classList.toggle('body-active');
+ }, 5000);
+*/
 //нужно сделать, чтобы background у body менялся через 5 секунд
 
 header.append("TODO LIST/tms edition");
@@ -17,9 +27,15 @@ inputButton.append("Add");
 //дописать onclick, чтобы если меньше чем 2, то ничего не делать и вывести alert c ошибка
 //по нажатию на кнопку будет меняться ее цвет
 inputButton.onclick = function () {
+  if (input.value.length > 2) {
   addToList(input.value);
   input.value = "";
+  inputButton.classList= ("button-active");
+  } else {
+    alert('Error');
+  }
 };
+
 
 let ol = document.createElement("ol");
 
@@ -28,26 +44,22 @@ function addToList(text) {
 
   let removeButton = document.createElement("button");
   removeButton.append("Remove");
+  
 
   let doneButton = document.createElement("button");
   doneButton.append("Done");
+  
 
   let p = document.createElement("p");
   p.append(text);
 
   //заменить cssText на классы
+  p.classList = ("p-class");
 
-  p.style.cssText = `
-    font-size: 20px;
-    margin: 0 10px;
-  `;
 
   let liContainer = document.createElement("div");
 
-  liContainer.style.cssText = `
-    display: flex;
-  `;
-
+  liContainer.classList = ("container");
   liContainer.append(doneButton, p, removeButton);
   li.append(liContainer);
 
