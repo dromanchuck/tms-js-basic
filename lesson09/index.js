d3e713bb4fc92cdcbd7eda24b091a5cc273766f6
Dmitry <d.ramanchuk@arateg.com>
2020-07-22 20:14:50 +0300

    Add fixes

--- lesson09/index.js
+++ lesson09/index.js
@@ -1,7 +1,17 @@
 let container = document.createElement("div");
 let header = document.createElement("h1");
+document.body.classList.add("container");
+let localTodos = localStorage.getItem("todos");
+let todos = JSON.parse(localTodos) || [];
 
-//нужно сделать, чтобы background у body менялся через 5 секунд
+setInterval(() => {
+  if (document.body.classList.contains("second-container")) {
+    document.body.classList.remove("second-container");
+    return;
+  }
+
+  document.body.classList.add("second-container");
+}, 5000);
 
 header.append("TODO LIST/tms edition");
 container.append(header);
@@ -14,43 +24,85 @@ input.type = "text";
 
 const inputButton = document.createElement("button");
 inputButton.append("Add");
-//дописать onclick, чтобы если меньше чем 2, то ничего не делать и вывести alert c ошибка
-//по нажатию на кнопку будет меняться ее цвет
+
 inputButton.onclick = function () {
-  addToList(input.value);
-  input.value = "";
+  if (input.value.trim().length > 2) {
+    addToList(input.value);
+    input.value = "";
+    inputButton.setAttribute("class", "button-active");
+
+    setTimeout(() => {
+      inputButton.classList.remove("button-active");
+    }, 1000);
+  } else {
+    alert("Введите больше чем 2 символа");
+  }
 };
 
 let ol = document.createElement("ol");
 
 function addToList(text) {
+  let id = "_" + Math.random().toString(36).substr(2, 9);
   let li = document.createElement("li");
+  let p = document.createElement("p");
+  let obj = { text, id, done: false };
+  todos.push(obj);
+
+  localStorage.setItem("todos", JSON.stringify(todos));
 
   let removeButton = document.createElement("button");
   removeButton.append("Remove");
 
   let doneButton = document.createElement("button");
   doneButton.append("Done");
+  //дописать взаимодествие с localStorage
+  doneButton.onclick = () => {
+    if (p.matches(".text-done")) {
+      p.classList.remove("text-done");
+      doneButton.innerHTML = "Done";
 
-  let p = document.createElement("p");
+      todos.forEach((elem, index, arr) => {
+        if (elem.id === id) {
+          arr[index].done = false;
+        }
+      });
+    } else {
+      p.classList.add("text-done");
+      doneButton.innerHTML = "To Do";
+
+      todos.forEach((elem, index, arr) => {
+        if (elem.id === id) {
+          arr[index].done = true;
+        }
+      });
+    }
+
+    console.log(todos);
+  };
+  //дописать взаимодествие с localStorage
+  removeButton.onclick = function () {
+    let filteredTodos = todos.filter((elem) => {
+      if (elem.id === id) {
+        return false;
+      }
+
+      return true;
+    });
+
+    todos = [...filteredTodos];
+    li.remove();
+  };
+
   p.append(text);
-
-  //заменить cssText на классы
-
-  p.style.cssText = `
-    font-size: 20px;
-    margin: 0 10px;
-  `;
+  p.className = "todo-text";
 
   let liContainer = document.createElement("div");
 
-  liContainer.style.cssText = `
-    display: flex;
-  `;
+  liContainer.classList.add("wrapper");
 
   liContainer.append(doneButton, p, removeButton);
   li.append(liContainer);
-
+  li.id = id;
   ol.append(li);
 }
 
@@ -58,3 +110,18 @@ inputContaner.append(input, inputButton);
 header.after(inputContaner);
 inputContaner.after(ol);
 document.body.append(container);
+
+todos.forEach((elem) => {
+  let li = document.createElement("li");
+  let p = document.createElement("p");
+  let liContainer = document.createElement("div");
+
+  liContainer.classList.add("wrapper");
+
+  p.append(elem.text);
+  p.className = "todo-text";
+  liContainer.append(p);
+  li.append(liContainer);
+
+  ol.append(li);
+});
