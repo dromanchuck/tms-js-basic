21b5da631988b2f21d7e4c2084ae4866cd382559
Dmitry <d.ramanchuk@arateg.com>
2020-07-28 12:22:45 +0300

    Add tasks lesson 12

---
+++ lesson12/homeWork.js
@@ -0,0 +1,13 @@
+/**
+ * Решить задания
+ * https://learn.javascript.ru/task/async-from-regular
+  https://learn.javascript.ru/task/rewrite-async
+  http://new.code.mu/ru/javascript/book/promise/resolve-reject/
+ */
+
+/**
+ * Задание со зведочкой
+ *
+ * Переписать дз к уроку 11 используя async/await вместо промисов
+ *
+ */
