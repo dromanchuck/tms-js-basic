ebbf45179fe1ba948f3ef1277dceaffefb11a0ce
Your Name <you@example.com>
2020-08-01 10:27:41 +0300

    first_commit

--- lesson12/homeWork.js
+++ lesson12/homeWork.js
@@ -11,3 +11,29 @@
  * Переписать дз к уроку 11 используя async/await вместо промисов
  *
  */
+
+/* async function wait() {
+ await new Promise(resolve => setTimeout(resolve, 1000));
+ return 10;
+}
+
+function f() {
+ wait().then(resolve => console.log(resolve));
+}
+f(); */
+
+/* async function loadJson(url) {
+ response = await fetch(url);
+ if (response.status == 200) {
+  let value = await response.json();
+  return value;
+ } else {
+  throw new Error(response.status);
+ }
+};
+loadJson('https://jsonplaceholder.typicode.com/posts')
+ .catch(alert); */
+
+let getRandomNumber = () => {
+ return Math.ceil(Math.random() * 10);
+};
