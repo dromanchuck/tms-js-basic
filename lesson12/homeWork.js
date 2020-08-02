ee1b908820dba3096597d0b945a97423b51aaf6a
Your Name <you@example.com>
2020-08-02 16:25:44 +0300

    first_commit

--- lesson12/homeWork.js
+++ lesson12/homeWork.js
@@ -11,8 +11,8 @@
  * Переписать дз к уроку 11 используя async/await вместо промисов
  *
  */
-
-/* async function wait() {
+//1
+async function wait() {
  await new Promise(resolve => setTimeout(resolve, 1000));
  return 10;
 }
@@ -20,9 +20,9 @@
 function f() {
  wait().then(resolve => console.log(resolve));
 }
-f(); */
-
-/* async function loadJson(url) {
+f();
+//2
+async function loadJson(url) {
  response = await fetch(url);
  if (response.status == 200) {
   let value = await response.json();
@@ -32,8 +32,44 @@ f(); */
  }
 };
 loadJson('https://jsonplaceholder.typicode.com/posts')
- .catch(alert); */
+ .catch(alert);
 
-let getRandomNumber = () => {
- return Math.ceil(Math.random() * 10);
-};
+//first 3 task сode
+let promise = new Promise((resolve, reject) => {
+ let timeout = Math.ceil(Math.random() * 10);
+ setTimeout(() => {
+  resolve('hello')
+  if (timeout <= 5 && timeout >= 1) {
+   resolve(timeout);
+  } else {
+   reject(new Error("время вышло!"));
+  }
+ }, timeout);
+
+});
+
+promise
+ .then(
+  result => alert("Fulfilled: " + result),
+  error => alert("Rejected: " + error.message) // Rejected: время вышло!
+ );
+
+//second
+let timeout = Math.ceil(Math.random() * 10);
+const delay = ms => new Promise(resolve => setTimeout(resolve, ms * 1000));
+
+async function something() {
+ await delay(timeout);
+ try {
+  if (timeout <= 5 && timeout >= 1) {
+   console.log(timeout);
+   return timeout;
+  } else {
+   console.log(timeout);
+   throw new Error("Ошибка");
+  }
+ } catch (e) {
+  return e;
+ }
+}
+something();
