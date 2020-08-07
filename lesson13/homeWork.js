315729a2ef73e62373fc05621c91289877461dab
MichaelZavalo <mihailzavalo@yandex.ru>
2020-08-07 11:48:43 +0300

    add homework 13

--- lesson13/homeWork.js
+++ lesson13/homeWork.js
@@ -7,6 +7,20 @@
  * Зарплата - это произведение (умножение) ставки rate на количество отработанных дней days.
  */
 
+function Worker(name,secondName,rate,days) {
+    this.name = name;
+    this.secondName = secondName;
+    this.rate = rate;
+    this.days = days;
+}
+
+Worker.prototype.getSalary = function() {
+    return this.rate * this.days;
+}
+
+let worker = new Worker('hi','hello',123,223);
+console.log(worker);
+
 /**
  * Задание 1
  * Модифицируйте функцию конструктор Worker из предыдущей задачи следующим образом:
@@ -15,8 +29,43 @@
  * И также методы setRate, setDays, которые будут устанавливать новые значения для соответственно свойств rate и days.
  */
 
+Worker.prototype.getName = function() {
+    return this.name;
+}
+
+Worker.prototype.getSecondName = function() {
+    return this.secondName;
+}
+
+Worker.prototype.getRate = function() {
+    return this.rate;
+}
+
+Worker.prototype.getDays = function() {
+    return this.days;
+}
+
+Worker.prototype.setRate = function(rate) {
+    if(isNaN(Number(rate))) {
+        return true;
+    } else {
+        this.rate = rate;
+    }
+}
+
+Worker.prototype.setDays = function(days) {
+    if(isNaN(Number(days))) {
+        return true;
+    } else {
+        this.days = days;
+    }
+}
+
+worker.setRate(1000);
+worker.setDays(1);
+
 /**
- * Задание 1
+ * Задание 2
  * Реализуйте функцию конструктор MyString, объект которого будет иметь следующие методы: метод reverse(),
  * который параметром принимает строку, а возвращает ее в перевернутом виде,
  * метод ucFirst(),
@@ -24,3 +73,29 @@
  * и метод ucWords,
  * который принимает строку и делает заглавной первую букву каждого слова этой строки.
  */
+
+function MyString() {}
+
+MyString.prototype.reverse = function(str) {
+    let arr = [...str].reverse();
+
+    return arr.join('');
+}
+
+MyString.prototype.upperCaseFirst = function(str) {
+    let arr = [...str];
+    arr[0] = arr[0].toUpperCase();
+    return arr.join('');
+}
+
+MyString.prototype.ucWords = function(str) {
+    let arr = str.split(' ');
+    arr.forEach((element,ind,arr) => {
+        arr[ind] = this.upperCaseFirst(element);
+    });
+    return arr.join(' ');
+}
+
+let string = new MyString();
+
+console.log(string.ucWords('Why are you mad?'));
