b9717ac54414d1b18758cd75181c7d23a44ecd31
MichaelZavalo <mihailzavalo@yandex.ru>
2020-08-07 11:41:49 +0300

    add homework 11

--- lesson12/homeWork.js
+++ lesson12/homeWork.js
@@ -1,9 +1,14 @@
 /**
  * Решить задания
- * https://learn.javascript.ru/task/async-from-regular
-  https://learn.javascript.ru/task/rewrite-async
-  http://new.code.mu/ru/javascript/book/promise/resolve-reject/
- */
+/* https://learn.javascript.ru/task/async-from-regular */
+
+
+
+
+/* https://learn.javascript.ru/task/rewrite-async */
+
+/* http://new.code.mu/ru/javascript/book/promise/resolve-reject/ */
+/* let getRandomNumber = function  */
 
 /**
  * Задание со зведочкой
@@ -11,3 +16,51 @@
  * Переписать дз к уроку 11 используя async/await вместо промисов
  *
  */
+
+
+let user = {
+    name: "Andrue",
+    secondName: "Pupkin",
+    email: "mail.@mail.ru",
+ 
+    sayHello() {
+ console.log(`Hello, my name is ${this.getFullName()}`);
+    },
+    
+    setName(name) {
+    this.name = name;
+    },
+    
+    getFullName() {
+      return `${this.name} ${this.secondName}`;
+    }
+ };
+ 
+ user.sayHello();
+ user.setName('Antony');
+ user.sayHello();
+
+
+ function User(name,secondName,email) {
+    this.name = name;
+    this.secondName = secondName;
+    this.email = email;
+    
+    this.sayHello = function() {
+  console.log(`Hello, my name is ${this.getFullName()}`);
+     }
+     
+     this.setName = function(name) {
+     this.name = name;
+     }
+     
+     this.getFullName = function() {
+       return `${this.name} ${this.secondName}`;
+     }
+  }
+  
+  let vasya = new User('Vasia', 'Dupkin', 'tetet@google.com');
+  let maria = new User('Maria', 'Dupkin', 'tetet2@google.com');
+  
+  
+  vasya.sayHello();
