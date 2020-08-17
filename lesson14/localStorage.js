8b20ad016e6b6ebbd7022f60386de8409e5d94ca
MichaelZavalo <mihailzavalo@yandex.ru>
2020-08-17 17:40:26 +0300

    lesson 15

---
+++ lesson14/localStorage.js
@@ -0,0 +1,14 @@
+export class Local {
+    static get todos() {
+        let todosStr = localStorage.getItem('todos');
+        return JSON.parse(todosStr);
+    }
+
+    static set todos(todosArr) {
+        localStorage.setItem('todos', JSON.stringify(todosArr));
+    }
+}
+
+
+
+export let num = 10;
