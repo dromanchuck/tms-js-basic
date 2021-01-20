6774ccfe4e644f301b378575da0b33137337a8c7
Yauheni Bandyukevich <zhenya.bandyukevich@mail.ru>
2021-01-20 19:33:40 +0300

    homework done

--- lesson03/homeWork.js
+++ lesson03/homeWork.js
@@ -13,6 +13,14 @@
  * объект должен содержать следущие поля: имя, email, нравятся ли ему фрукты, показатель IQ.
  * Созданный объект вывести в консоли.
  */
+const user = {
+  name: 'Petr',
+  email: 'petr@mail.com',
+  likeFruits: true,
+  iq: 33,
+};
+
+console.log(user);
 
 /**
  * Задание 1
@@ -25,7 +33,16 @@
  * Необходимо вывести в консоль объект, а затем "укоротить" дерево, и вывести в консоль.
  * Подсказка: посмотреть операторы += -=
  */
+const tree = {
+  name: 'applewood',
+  height: 60,
+  leaves: 10000,
+  family: 'family1',
+}
 
+tree.height -= 20;
+
+console.log(tree)
 /**
  * Задание 2
  * Напишите цикл от 0 до 15. И в каждой итерации цикла нужно проверить четное число или нечетное.
@@ -34,6 +51,14 @@
  * 1 это нечетное
  * ...
  */
+for (let i = 0; i <= 15; i++) {
+  if (i % 2 === 0) {
+    console.log(`${i} это чётное`)
+  }
+  if (i % 2 !== 0) {
+    console.log(`${i} это нечётное`)
+  }
+}
 
 /**
  * Задание 3
@@ -51,6 +76,30 @@
  * Зина - 3
  * ...
  */
+const studentsMarks = {
+  'Вася': 80,
+  'Зина': 77,
+  'Катя': 88,
+  'Петя': 95,
+  'Вова': 57,
+}
+
+console.log(studentsMarks)
+
+for (let key in studentsMarks) {
+  if (studentsMarks[key] > 90) {
+    console.log(`${key} - 5`)
+  }
+  else if (studentsMarks[key] > 80) {
+    console.log(`${key} - 4`)
+  }
+  else if (studentsMarks[key] > 70) {
+    console.log(`${key} - 3`)
+  }
+  else if (studentsMarks[key] > 60) {
+    console.log(`${key} - 2`)
+  }
+}
 
 /**
  * Задание 4
@@ -60,6 +109,17 @@
  * если число кратное 5 - то вывести в консоль строку Buzz,
  * если число кратное и 3 и 5 - то вывести FizzBuzz
  */
+for (let i = 1; i <= 100; i++) {
+  if (i % 3 === 0 && i % 5 !== 0) {
+    console.log('Fizz')
+  }
+  else if (i % 5 === 0 && i % 3 !== 0) {
+    console.log('Buzz')
+  }
+  else if (i % 3 === 0 && i % 5 === 0) {
+    console.log('FizzBuzz')
+  }
+}
 
 /**
  * Задание 5
@@ -73,18 +133,44 @@
 //**
 //***
 //****
+let result = '';
+for (let i = 0; i < 7; i++) {
+  result += '*'
+  console.log(result);
+}
 
 /**
  * Задание 6
  * Написать цикл от 0 до 1000. Высчитать сумму всех чисел кратных 4 и сумму всех чисел кратных 3.
  *
  */
+let sum4 = 0;
+let sum3 = 0;
+for (let i = 0; i < 1000; i++) {
+  if (i % 4 === 0) {
+    sum4 += i;
+  } else if (i % 3 === 0) {
+    sum3 += i;
+  }
+}
+console.log(`Сумма чисел кратных 4 = ${sum4}`)
+console.log(`Сумма чисел кратных 3 = ${sum3}`)
 
 /**
  * Задание 7
  * Написать программу, которая найдет наибольший общий делитель двух целых чисел и вывести это число в alert.
  *
  */
+function nod(a, b) {
+  while (a != 0 && b != 0) {
+    if (a > b) {
+      a = a % b;
+    } else {
+      b = b % a;
+    }
+  }
+  return a + b;
+}
 
 /**
  * Задание 8
@@ -93,3 +179,5 @@
  * Склонированный массив вывести в консоль
  *
  */
+const array1 = [1, 2, 3, 4, 5]
+const array2 = [...array1]
