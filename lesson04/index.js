6c528580efe4e5ddf3142f04e2a323bb51c6fe56
Dmitry <d.ramanchuk@arateg.com>
2021-01-21 21:40:07 +0300

    Fix

--- lesson04/index.js
+++ lesson04/index.js
@@ -1,129 +1,174 @@
-//Arrays
+//function
+function nod(a, b) {
+  while (a != 0 && b != 0) {
+    if (a > b) {
+      a = a % b;
+    } else {
+      b = b % a;
+    }
+  }
 
-let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-let array1 = ["1", "123", "hello", "world"];
-let array2 = [1, 2, "123"];
-let array3 = [{ a: "Test", b: "test" }, [1, 2, 3, 4, 5, 6, 7], "123", 123];
+  return a + b;
+}
 
-//${array_name}.length - узнать длинну массива
+let nod1 = function (a, b) {
+  nod(a, b);
+};
 
-//${array_name}[index] - обратиться к элементу массива
+let nod2 = (a, b) => {
+  node(a, b);
+};
 
-let array = [19, 87, 765, 459, 12, -123];
+console.log(nod(24, 12));
+let result = nod(24, 13);
 
-for (let i = 0; i <= array.length - 1; i++) {
-  if (array[i] < 0) {
-    console.log(`${array[i]} - < 0`);
-  } else {
-    console.log(`${array[i]} - > 0`);
+//array
+
+let array = [1, 2, 3, 4, 5];
+
+//concat
+
+let a1 = [1, 2, 3, 4];
+let a2 = [5, 6, 7, 8];
+
+let a3 = a1.concat(a2);
+
+//push
+array.push(10); //[1, 2, 3, 4, 5, 10]
+
+//pop
+array.pop(); //[1, 2, 3, 4, 5]
+
+//unshift
+array.unshift(0); //[0, 1, 2, 3, 4, 5]
+
+//shift
+array.shift(); //[1, 2, 3, 4, 5]
+
+//sort
+
+let unsortedArray = [5, 3, 7, 10, -10, 89, 87];
+
+let sortedArray1 = unsortedArray.sort(function (a, b) {
+  // от меньшего к большему
+  return a - b;
+});
+
+let sortedArray2 = unsortedArray.sort(function (a, b) {
+  // от большего к меньшему
+  return b - a;
+});
+
+//bubble sort
+
+let array1 = [5, 3, 7, 10, -10, 89, 87];
+let temp;
+
+for (let i = 0; i < array1.length; i++) {
+  for (let j = 0; j < array1.length; j++) {
+    if (!array1[j + 1]) {
+      continue;
+    }
+    console.log(array1[j], array1[j + 1]);
+    if (array1[j] > array1[j + 1]) {
+      temp = array1[j + 1];
+      array1[j + 1] = array1[j];
+      array1[j] = temp;
+    }
   }
 }
 
-//сортировка пузырьком
-let array = [19, 87, 765, 459, 12, -123];
+//for ... of
 
-//1: [19, 87, 459, 12, -123, 765];
-//2: [19, 87, 12,-123,459, 765]
+for (let value of array1) {
+  console.log(value);
+}
 
-for (let i = 0; i <= array.length - 1; i++) {
-  for (let j = 0; j <= array.length - 1; j++) {
-    if (array[j] > array[j + 1]) {
-      // j === 0 array[0] - 19 vs array[1] - 87
+//forEach
 
-      let temp = array[j + 1];
-      array[j + 1] = array[j];
-      array[j] = temp;
+let array1 = [5, 3, 7, 10, -10, 89, 87];
+let a1 = [];
+let a2 = [];
+
+array1.forEach(function (value) {
+  if (value % 2 === 0) {
+    a1.push(value);
+  } else {
+    a2.push(value);
+  }
+});
+
+//forEach bubble sort
+
+let temp;
+
+array1.forEach(function () {
+  array1.forEach(function (innerValue, index, arr) {
+    if (arr[index] > arr[index + 1]) {
+      temp = arr[j + 1];
+      arr[j + 1] = arr[j];
+      arr[j] = temp;
+    }
+  });
+});
+
+//map
+
+let array1 = [5, 3, 7, 10, -10, 89, 87];
+let array2 = [1, 2, 3, 4, 5, 6, 7];
+
+let a = array1.map(function (value, index) {
+  return value + array2[index];
+});
+
+//filter
+
+let array1 = [5, 3, -7, 10, -10, 89, -87];
+
+let filteredArray = array1.filter((value) => {
+  return value > 0;
+});
+
+//reduce
+
+let food = [
+  { name: "Gamburger", id: 0, servingSize: 100, price: 10 },
+  { name: "Cheeseburger", id: 1, servingSize: 120, price: 15 },
+  { name: "Kartoha", id: 2, servingSize: 100, price: 5 },
+  { name: "Pizza", id: 3, servingSize: 100, price: 90 },
+];
+
+let result = food.reduce(
+  (acc, value, index) => {
+    acc.total += value.price;
+    acc.receipt += `${value.name} - ${value.servingSize}g - ${value.price}$ \n`;
+
+    if (index === food.length - 1) {
+      acc.receipt += `*****Total: ${acc.total}$*****`;
     }
 
-    console.log(array);
-  }
-}
+    return acc;
+  },
+  { receipt: "", total: 0 }
+);
 
-//array.push(element) - добавляет элемент в конец массива
-let arr = [1, 2, 3];
-console.log(arr); //[1,2,3]
+console.log(result.receipt);
 
-arr.push(4);
+//slice
+console.log("asd" === "asd");
+let testString = "Hello JS";
 
-console.log(arr); //[1,2,3,4]
+let str = testString.slice(1, 10);
 
-//array.unshift(element) - добавляет элемент в начало массива
-let arr = [1, 2, 3];
-console.log(arr); //[1,2,3]
+//split
 
-arr.unshift(4);
+console.log("asd" === "asd");
 
-console.log(arr); //[4,1,2,3]
+let testString = "hello";
+let array = testString.split("");
 
-//array.pop() - убирает последний элемент массива
+let newArray = array.map((value, index) =>
+  index === 0 ? value.toUpperCase() : value
+);
 
-let arr = [1, 2, 3];
-console.log(arr); //[1,2,3]
-
-arr.pop();
-
-console.log(arr); //[1,2]
-
-//array.shift() - убирает нулевой элемент массива
-
-let arr = [1, 2, 3];
-console.log(arr); //[1,2,3]
-
-arr.shift();
-
-console.log(arr); //[2,3]
-
-//array.splice(${индекс}, ${количество элементов}) - используется для удаления части массива
-
-let arr = [1, 2, 3];
-
-let newArr = arr.splice(0, 2); // вызов splice "вырежет" из исходного массива arr [1,2] и вернет эту часть, что в свою очередь сохранится в newArr
-
-console.log(arr); //[3]
-console.log(newArr); //[1,2]
-
-//array.slice(${начальный индекс}, ${конечный индекс}) - скопирует часть массива, при этом не модифицируя исходный массив,
-//начиная от начального индекса
-//не включая конечный индекс
-let arr = [1, 2, 3, 4, 5];
-
-let secondArr = arr.slice(); //скопирует массив полностью
-let thirdArr = arr.slice(1); // скопирует массив начинания с первого элемента
-console.log(thirdArr); // [2,3]
-
-let fourthArr = arr.slice(1, 4); // скопирует массив начинания с первого элемента заканчивая 3м
-
-console.log(fourthArr); //[2, 3, 4]
-
-//string.split(разделитель) - разделяет строку и возвращает массив и символов
-
-let str = "Hello World";
-
-let arr = str.split("");
-console.log(arr); // ["H", "e", "l", "l", "o", " ", "W", "o", "r", "l", "d"]
-
-let arr1 = str.split(" ");
-console.log(arr1); //["Hello", "World"]
-
-//arr.join(разделитель) - объединит массив в строку
-
-let arr = [1, 2, 3, 4, 5];
-
-let str = arr.join();
-console.log(str); // '1,2,3,4,5'
-
-let str1 = arr.join("");
-console.log(str1); // '12345'
-
-let str2 = arr.join(" ");
-console.log(str2); // '1 2 3 4 5'
-
-//indexOf
-
-let array = [1, 2, 3, 4, 5, 6];
-
-let indexOfThree = array.indexOf(3);
-
-console.log(indexOfThree);
-
-//includes
+let resultString = newArray.join("");
