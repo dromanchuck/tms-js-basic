376d30f883b4e701ba5af1479154cfd859869323
Dmitry <d.ramanchuk@arateg.com>
2021-01-25 21:30:35 +0300

    Fix index

--- lesson05/index.js
+++ lesson05/index.js
@@ -51,19 +51,3 @@ let user = {};
 
 user.name = "Jack";
 user.secondName = "Sparrow";
-
-//arguments
-function findMax() {
-  let args = [...arguments];
-  console.log(args, arguments);
-
-  let max = args.reduce((acc, value) => {
-    return acc > value ? acc : value;
-  }, args[0]);
-
-  return max;
-}
-
-let max = findMax(1, 2, 3, 4, 5, 6, 7, 8, 9);
-
-//JSON parse stringify
