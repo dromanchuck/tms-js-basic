1fdd2ef39757de6baebb3208405762fcc3c48d9b
Ilya <ilya.naumenko53@gmail.com>
2021-01-25 12:54:25 +0300

    Merge

--- lesson03/homeWork.js
+++ lesson03/homeWork.js
@@ -14,6 +14,15 @@
  * Созданный объект вывести в консоли.
  */
 
+let user = {
+  name: 'Ilya',
+  email: 'Ilya@gmail.com',
+  likesFruits: true,
+  iq: 200,
+}
+
+console.log(user)
+
 /**
  * Задание 1
  * Создайте объект дерево у которого есть следующие свойства:
@@ -26,6 +35,19 @@
  * Подсказка: посмотреть операторы += -=
  */
 
+let tree = {
+  name: 'pine',
+  height: 100,
+  leavesNumber: 1000,
+  family: 'pine family',
+}
+
+console.log(tree)
+
+tree.height -= 20
+
+console.log(tree)
+
 /**
  * Задание 2
  * Напишите цикл от 0 до 15. И в каждой итерации цикла нужно проверить четное число или нечетное.
@@ -34,6 +56,18 @@
  * 1 это нечетное
  * ...
  */
+
+const evenOrNot = () => {
+  for (let i = 0; i <= 15; i++) {
+    if (i % 2 === 0) {
+      console.log(`${i} это чётное число.`)
+    } else {
+      console.log(`${i} это нечётное число.`)
+    }
+  }
+}
+
+evenOrNot()
 
 /**
  * Задание 3
@@ -52,6 +86,32 @@
  * ...
  */
 
+let students = {
+  Вася: 80,
+  Зина: 77,
+  Катя: 88,
+  Петя: 95,
+  Вова: 57,
+}
+
+const finalMarks = () => {
+  for (let key in students) {
+    if (students[key] > 90) {
+      console.log(`${key} - 5`)
+    } else if (students[key] > 80) {
+      console.log(`${key} - 4`)
+    } else if (students[key] > 70) {
+      console.log(`${key} - 3`)
+    } else if (students[key] > 60) {
+      console.log(`${key} - 2`)
+    } else if (students[key] > 50) {
+      console.log(`${key} - 1`)
+    }
+  }
+}
+
+finalMarks()
+
 /**
  * Задание 4
  * Написать цикл, который будет итерироватся от 1 до 100.
@@ -61,6 +121,20 @@
  * если число кратное и 3 и 5 - то вывести FizzBuzz
  */
 
+const fizzBuzz = () => {
+  for (let i = 0; i <= 1000; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log('FizzBuzz')
+    } else if (i % 3 === 0) {
+      console.log('Fizz')
+    } else if (i % 5 === 0) {
+      console.log('Buzz')
+    }
+  }
+}
+
+// fizzBuzz()
+
 /**
  * Задание 5
  *
@@ -68,6 +142,17 @@
  * Для рисования использовать символ *(звездочка) или %(знак процента) и консоль.
  *
  */
+
+let percent = '%'
+
+const triangle = () => {
+  while (percent.length <= 4) {
+    console.log(percent)
+    percent += '*'
+  }
+}
+
+triangle()
 
 //*
 //**
@@ -80,11 +165,48 @@
  *
  */
 
+let sumFourth = 0
+let sumThree = 0
+
+const sum = () => {
+  for (let i = 0; i <= 1000; i++) {
+    if (i % 4 === 0) {
+      sumFourth = +i
+    }
+    if (i % 3 === 0) {
+      sumThree = +i
+    }
+  }
+}
+
+sum()
+
+console.log(`Сумма чисел кратных 4 равна ${sumFourth}`)
+console.log(`Сумма чисел кратных 3 равна ${sumThree}`)
+
 /**
  * Задание 7
  * Написать программу, которая найдет наибольший общий делитель двух целых чисел и вывести это число в alert.
  *
  */
+
+const egcd = (a, b) => {
+  if (a === 0) {
+    return b
+  }
+
+  while (b != 0) {
+    if (a > b) {
+      a = a - b
+    } else {
+      b = b - a
+    }
+  }
+
+  return a
+}
+
+console.log(egcd(4, 16))
 
 /**
  * Задание 8
@@ -93,3 +215,9 @@
  * Склонированный массив вывести в консоль
  *
  */
+
+let arr = [1, 5, 8, 10, 87]
+
+let arr2 = { ...arr }
+
+console.log(arr2)
