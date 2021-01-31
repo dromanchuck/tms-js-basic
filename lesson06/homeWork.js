49c363032a4448608011e2ed3e3245a0c957363f
Yauheni Bandyukevich <zhenya.bandyukevich@mail.ru>
2021-01-31 18:25:04 +0300

    HW done

--- lesson06/homeWork.js
+++ lesson06/homeWork.js
@@ -13,12 +13,43 @@
  * вторым и третьим аргументом функции(функции могут делать все что угодно на ваш выбор).
  * Внутри если условие равно true, то выполнить первую функцию, если false, то выполнить вторую функцию.
  */
+function task1(condition, func1, func2) {
+  if (condition) {
+    func1()
+  } else {
+    func2()
+  }
+}
+
+function func1() {
+  console.log('true function');
+}
+
+function func2() {
+  console.log('false function');
+}
+
+console.log(task1(2 === 2, func1, func2));
 
 /**
  * Задание 2
  * Написать функцию сравнения двух массивов, которая возвращает true или false в зависимости от того,
  * одинаковые у них элементы или нет.
  */
+const arr1 = [1, 2, 3, 4, 5]
+const arr2 = [1, 2, 3, 4, 5]
+function compareArrays(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false
+    }
+  }
+  return true;
+}
+console.log(compareArrays(arr1, arr2))
 
 /**
  * Задание 3
@@ -26,12 +57,30 @@
  * (Использовать замыкание)
  *
  */
+function countCalls() {
+  let count = 0;
+  return function func() {
+    return (count++) ** 2;
+  }
+}
+
+const innerFunction = countCalls();
+console.log(innerFunction());
+console.log(innerFunction());
+console.log(innerFunction());
+console.log(innerFunction());
+
 
 /**
  * Задание 4
  * Написать функцию, которая выводит количество миллисекунд с начала дня. За начало дня принимаем текущую дату и время 00:00.
  *
  */
+function getMiliseconds() {
+  const date = new Date();
+  date.setHours(0, 0, 0, 0);
+  return Date.now() - date.getTime();
+}
 
 /**
  * Задание 5
@@ -39,13 +88,20 @@
  * Для 2021 это будет 5.
  *
  */
+function sumDigits(value = 2021) {
+  return String(value).split('').reduce((acc, value) => +acc + +value)
+}
+
+console.log(sumDigits())
 
 /**
- * Задание 6
+ * Задание 6 ?
  * Описание задачи: Напишите функцию, которая разделит массив на части заданного размера и
  * вернет массив элементами которого являются эти части.
- * Пример функции: splitArray([1, 2, 3, 4, 2) => [[1, 2], [3, 4]]
+ * Пример функции: splitArray([1, 2, 3, 4], 2) => [[1, 2], [3, 4]]
  */
+
+ // не получается решить эту задачу
 
 /**
  * Задание 7
@@ -53,7 +109,12 @@
  * таких как false, undefined, пустые строки, 0, null.
  *
  */
+const arr = [false, undefined, 5, 0, null, 6, 85, '']
 
+function filterArray(arr) {
+  return arr.filter((value) => value)
+}
+console.log(filterArray(arr))
 /**
  * Задание 8 ****
  * Напишите функцию, которая принимает сколько угодное количество массивов и возвращает массив состоящий из .
@@ -62,3 +123,12 @@
  *  createArrayWithUniq([1, 2, 3], [2, 3], [2, 3, 5]) => [2, 3]
  *  createArrayWithUniq([1, 3], [2, 3], [2, 3, 5]) => [3]
  */
+
+function createArrayWithUniq(...args) {
+  return args.reduce((acc, oneArr) => {
+   return acc.filter((item) => oneArr.includes(item)) 
+  });
+}
+
+
+createArrayWithUniq([1, 3], [2, 3], [2, 3, 5])
