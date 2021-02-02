a8c1aaddd18d45211a71a0eb735882d3d96b5662
k0w4lk <k0w4lk@yandex.by>
2021-02-02 19:44:04 +0300

    Homework 7 done

--- lesson07/index.js
+++ lesson07/index.js
@@ -1,6 +1,6 @@
 function calcSum(number) {
   let numStr = String(number);
-  let arr = numStr.split("");
+  let arr = numStr.split('');
   let sum = 0;
 
   for (let i = 0; i < arr.length; i++) {
@@ -11,20 +11,20 @@ function calcSum(number) {
 }
 
 function test(message, result, expectedResult) {
-  let status = result === expectedResult ? " Успех" : " Ошибка";
+  let status = result === expectedResult ? ' Успех' : ' Ошибка';
   console.log(
     message +
-      "----" +
-      "Результат: " +
+      '----' +
+      'Результат: ' +
       result +
-      " ожидаемый результат " +
+      ' ожидаемый результат ' +
       expectedResult +
-      "   status" +
+      '   status' +
       status
   );
 }
 
-test("Число 2021 - должно вывести 5", calcSum(2021), 5);
-test("Число 2022 - должно вывести 6", calcSum(2022), 6);
-test("Число 2021 - должно вывести 5", calcSum(2021), 4);
-test("Число 2021 - должно вывести 5", calcSum(2021), 5);
+test('Число 2021 - должно вывести 5', calcSum(2021), 5);
+test('Число 2022 - должно вывести 6', calcSum(2022), 6);
+test('Число 2021 - должно вывести 5', calcSum(2021), 4);
+test('Число 2021 - должно вывести 5', calcSum(2021), 5);
