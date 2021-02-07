12d4bf01d0904493e482d9d500041ca620f60619
Anton <artsiushko.yl@mail.ru>
2021-02-07 14:48:37 +0300

    AntonA/lesson8

--- lesson08/homeWork.js
+++ lesson08/homeWork.js
@@ -9,6 +9,8 @@
 
 //Внимание! Верстку для заданий использовать файл index.html
 
+document.body.style.backgroundColor = 'white';
+
 /**
  * Задание №1
  * 
@@ -23,11 +25,18 @@
  *
  */
 
+document.getElementById('unordered_list'); // first
+document.querySelectorAll('div')[1]; // second
+document.querySelector('#unordered_list'); //third
+document.getElementsByClassName('ordered_list')[1]; //fourth
+
 /**
  * Задание №2
  * Получить кнопку с текстом click. Сделать так, чтобы по нажатию на кнопку вызывался alert('Hello!!!');
  *
  */
+
+document.querySelectorAll('button')[1].onclick = function() {alert('Hello!!!');}
 
 /**
  * Задание №3
@@ -35,12 +44,34 @@
  *
  */
 
+let arrButton = document.querySelectorAll('button');
+arrButton.forEach((item) => {
+  item.style.padding = '10px';
+  item.style.fontSize = '30px';  
+  item.style.textTransform = 'uppercase';
+});
+
 /**
  * Задание №4
  * Получить все элементы li c классом list_item. Изменить стиль(использовать inline стили и добавление класса).
  * Сделать все буквы маленькими у каждого текстого узла li. Добавить каждому li кнопку c текстом ok.
  *
  */
+
+let listItem = document.getElementsByClassName('list_item');
+let arrListItem = [...listItem];
+arrListItem.forEach((item) => {
+  item.style.textTransform = 'lowercase';
+
+  let btn = document.createElement('button');
+  btn.innerHTML = 'ok';
+  btn.style.marginLeft = '10px';
+  btn.style.padding = '0px';
+
+  item.append(btn); 
+});
+  
+
 
 /**
  * Задание 5
@@ -53,6 +84,57 @@
  *
  */
 
+ //createElem
+
+  let div = document.createElement('div');
+  let input = document.createElement('input');
+  let ul = document.createElement('ul');
+  let button = document.createElement('button');
+
+  button.innerHTML = 'нажми';
+
+//addEvent
+
+  button.onclick = () => {
+    let li = camelCase(input.value);
+    ul.append(li)
+  }
+
+//addElem
+
+  document.body.append(div);
+  div.append(input, button, ul);
+
+// function
+  
+  function camelCase(text) {
+    let li = document.createElement('li');
+    let useFunc = camelFunc(text);
+    let textCamelCase = document.createTextNode(useFunc);
+
+    li.append(textCamelCase);
+
+    return li;
+  }
+
+
+//camelCase
+
+  function camelFunc(str) {
+    let arr = str.toLowerCase().split(' ');
+    let arrFirstElem = arr[0];
+    let newArr = arr.filter((item) => item !== arr[0]);
+    let arrOtherElem = newArr.map((item) => upperElem(item)).reduce((total,current) => total + current,[]);
+    let lastArr = arrFirstElem.concat(arrOtherElem);    
+    return lastArr;
+    }
+    
+  function upperElem(str) {
+    let arrStr = str.split('');
+    arrStr.splice(0,1,arrStr[0].toUpperCase());
+    let result = arrStr.join('')
+    return result
+  }
 /**
  * Задание 6
  * Создать элементы 2 input'a и button добавить их в верстку файла index.html. Сделать так,
@@ -64,8 +146,45 @@
  * Инпут2: 'Hello'
  */
 
+//createElem
+
+let containerForExSix = document.createElement('div');
+let firstInput = document.createElement('input');
+let secondInput = document.createElement('input');
+let buttonWithInput = document.createElement('button');
+
+buttonWithInput.innerHTML = 'change';
+
+//addElem
+
+document.body.append(containerForExSix);
+document.body.append(firstInput, buttonWithInput, secondInput);
+
+//addEvent
+
+buttonWithInput.onclick = () => {
+  let changeInput = change();
+}
+
+//function
+
+function change() {
+  let temp;
+  temp = firstInput.value;
+  firstInput.value = secondInput.value;
+  secondInput.value = temp;
+}
+
+
 /**
  * Задание 7
  * Написать код, который будет каждую минуту будет менять цвет у страницы. Цвет должен генерироваться рандомно.
  *
  */
+
+function generateColor() {
+  return '#' + Math.floor(Math.random()*16777215).toString(16)
+} 
+
+let timerId = setInterval(() => document.body.style.background = generateColor(), 5000); //каждую минуту долго, для наглядности 5с
+ 
--- lesson08/index.js
+++ lesson08/index.js
@@ -1,153 +1,153 @@
-function createArr(...args) {
-  let [first, ...rest] = args;
+// function createArr(...args) {
+//   let [first, ...rest] = args;
 
-  return first.reduce((acc, curr) => {
-    let hasEvery = rest.every((item) => item.includes(curr));
-    // console.log(acc, curr, rest);
-    if (hasEvery) {
-      rest = rest.map((item) => {
-        let onlyOne = true;
-        return item.filter((value) => {
-          if (onlyOne) {
-            onlyOne = false;
-            return value !== curr;
-          }
+//   return first.reduce((acc, curr) => {
+//     let hasEvery = rest.every((item) => item.includes(curr));
+//     // console.log(acc, curr, rest);
+//     if (hasEvery) {
+//       rest = rest.map((item) => {
+//         let onlyOne = true;
+//         return item.filter((value) => {
+//           if (onlyOne) {
+//             onlyOne = false;
+//             return value !== curr;
+//           }
 
-          return true;
-        });
-      });
+//           return true;
+//         });
+//       });
 
-      acc.push(curr);
-    }
+//       acc.push(curr);
+//     }
 
-    return acc;
-  }, []);
-}
+//     return acc;
+//   }, []);
+// }
 
-// console.log(
-//   "First:",
-//   createArr([1, 2, 2, 3, 5], [2, 2, 3, 4, 5], [2, 2, 5, 4, 3])
-// );
-// console.log(
-//   "Second:",
-//   createArr([1, 2, 3, 5], [2, 2, 3, 4, 5], [2, 2, 5, 4, 3])
-// );
-// console.log("Third:", createArr([1, 2, 2, 3, 5], [2, 4, 5], [2, 2, 5, 4, 3]));
+// // console.log(
+// //   "First:",
+// //   createArr([1, 2, 2, 3, 5], [2, 2, 3, 4, 5], [2, 2, 5, 4, 3])
+// // );
+// // console.log(
+// //   "Second:",
+// //   createArr([1, 2, 3, 5], [2, 2, 3, 4, 5], [2, 2, 5, 4, 3])
+// // );
+// // console.log("Third:", createArr([1, 2, 2, 3, 5], [2, 4, 5], [2, 2, 5, 4, 3]));
 
-//DOM
+// //DOM
 
-// document.body.style.background = "white";
+// // document.body.style.background = "white";
+
+// // setTimeout(() => {
+// //   document.body.style.background = "white";
+// // }, 5000);
+
+// //getElementsByTagName
+
+// let li = document.getElementsByTagName("li");
+// let arrLi = [...li];
+
+// arrLi.forEach((item) => {
+//   item.style.fontFamily = "arial";
+// });
+
+// //getElementsByClassName
+
+// let liWithClass = document.getElementsByClassName("list_item");
+
+// //getElementById
+// let ul = document.getElementById("unordered_list");
+// /**
+//  * <div>
+//  *  <ul>
+//  *    li
+//  *    li
+//  *    li
+//  * </ul>
+//  * </div>
+//  */
+
+// //ul.innerHTML
+
+// /**
+//  * <ul>
+//  *    li
+//  *    li
+//  *    li
+//  * </ul>
+//  */
+
+// // ul.outerHTML
+
+// /**
+//  * <div>
+//  *  <ul>
+//  *    li
+//  *    li
+//  *    li
+//  * </ul>
+//  * </div>
+//  */
+
+// // querySelector & querySelectorAll
+
+// let lis = document.querySelectorAll("li");
+// let div = document.querySelector("div");
+
+// //navigation
+
+// document.body.childNodes; //всех детей вместе с текстовыми узлами
+
+// document.body.children; //всех детей
+
+// document.body.firstElementChild.nextElementSibling; //соседи
+// document.body.firstElementChild.previousElementSibling; //соседи
+
+// //document.createElement
+
+// let container = document.createElement("div");
+// let title = document.createElement("h1");
+// let subtitle = document.createElement("p");
+
+// //elem.append(someElem)
+
+// let body = document.body;
+// title.innerText = "TeachmeSkills / FE42";
+// subtitle.innerText = "Coded with love=)";
+
+// body.style = `
+//   display: flex;
+//   align-items: center;
+//   justify-content: center;
+//   height: 100vh;
+//   background: black;
+// `;
+
+// title.style = `
+//   color: #fff;
+//   font-family: arial;
+// `;
+
+// subtitle.style = `
+//   color: #fff;
+//   font-family: arial;
+// `;
+
+// container.style = `
+//   display: flex;
+//   flex-direction: column;
+//   align-items: center;
+// `;
+
+// body.append(container);
+
+// container.prepend(title);
+// container.append(subtitle);
 
 // setTimeout(() => {
-//   document.body.style.background = "white";
-// }, 5000);
+//   subtitle.remove();
+// }, 1000);
 
-//getElementsByTagName
-
-let li = document.getElementsByTagName("li");
-let arrLi = [...li];
-
-arrLi.forEach((item) => {
-  item.style.fontFamily = "arial";
-});
-
-//getElementsByClassName
-
-let liWithClass = document.getElementsByClassName("list_item");
-
-//getElementById
-let ul = document.getElementById("unordered_list");
-/**
- * <div>
- *  <ul>
- *    li
- *    li
- *    li
- * </ul>
- * </div>
- */
-
-//ul.innerHTML
-
-/**
- * <ul>
- *    li
- *    li
- *    li
- * </ul>
- */
-
-// ul.outerHTML
-
-/**
- * <div>
- *  <ul>
- *    li
- *    li
- *    li
- * </ul>
- * </div>
- */
-
-// querySelector & querySelectorAll
-
-let lis = document.querySelectorAll("li");
-let div = document.querySelector("div");
-
-//navigation
-
-document.body.childNodes; //всех детей вместе с текстовыми узлами
-
-document.body.children; //всех детей
-
-document.body.firstElementChild.nextElementSibling; //соседи
-document.body.firstElementChild.previousElementSibling; //соседи
-
-//document.createElement
-
-let container = document.createElement("div");
-let title = document.createElement("h1");
-let subtitle = document.createElement("p");
-
-//elem.append(someElem)
-
-let body = document.body;
-title.innerText = "TeachmeSkills / FE42";
-subtitle.innerText = "Coded with love=)";
-
-body.style = `
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 100vh;
-  background: black;
-`;
-
-title.style = `
-  color: #fff;
-  font-family: arial;
-`;
-
-subtitle.style = `
-  color: #fff;
-  font-family: arial;
-`;
-
-container.style = `
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
-
-body.append(container);
-
-container.prepend(title);
-container.append(subtitle);
-
-setTimeout(() => {
-  subtitle.remove();
-}, 1000);
-
-setTimeout(() => {
-  title.remove();
-}, 3000);
+// setTimeout(() => {
+//   title.remove();
+// }, 3000);
