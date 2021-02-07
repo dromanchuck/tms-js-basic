0b7810bc5bca6523cc0fb09cb434adf37b518e3c
Tatsiana1111 <tanyashket@gmail.com>
2021-02-07 13:07:40 +0300

    lesson8

--- lesson08/homeWork.js
+++ lesson08/homeWork.js
@@ -22,26 +22,45 @@
     тремя способами.
  *
  */
-
+let a = document.getElementById(`unordered_list`);
+console.log(a);
+let a = document.getElementsByClassName(`ordered_list`);
+console.log(a);
+let a = document.querySelectorAll(`div`);
+console.log(a);
 /**
  * Задание №2
  * Получить кнопку с текстом click. Сделать так, чтобы по нажатию на кнопку вызывался alert('Hello!!!');
  *
  */
-
+let a = document.getElementById("click");
+console.log(a)
 /**
  * Задание №3
  * Получить все кнопки из index.html. Изменить у каждой кнопки шрифт, размер и сделать в тексте все буквы большими (click -> CLICK);
  *
  */
-
+let a = document.getElementsByTagName("button");
+let arr = [...a];
+arr.forEach((item) => {
+  item.style.fontFamily = `Times New Roman`;
+  item.style.fontSize = `large`;
+  item.style.textTransform = `uppercase`;
+})
 /**
  * Задание №4
  * Получить все элементы li c классом list_item. Изменить стиль(использовать inline стили и добавление класса).
  * Сделать все буквы маленькими у каждого текстого узла li. Добавить каждому li кнопку c текстом ok.
  *
  */
-
+let elem = document.getElementsByClassName(`list_item`);
+let arr = [...elem];
+arr.forEach((item) => {
+  let button = document.createElement(`button`);
+  item.style.textTransform = `lowercase`;
+  item.append(button);
+  button.innerText = `OK`;
+})
 /**
  * Задание 5
  * Создать элементы input и button добавить их в верстку файла index.html. Сделать так. чтобы по нажатию на
@@ -69,3 +88,9 @@
  * Написать код, который будет каждую минуту будет менять цвет у страницы. Цвет должен генерироваться рандомно.
  *
  */
+setInterval(() => {
+  let a = Math.floor(Math.random() * 255);
+  let b = Math.floor(Math.random() * 255);
+  let c = Math.floor(Math.random() * 255);
+  document.body.style.backgroundColor = "rgb(" + a + "," + b + "," + c +")";
+}, 60000);
