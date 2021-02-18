7da3782e3e0dafbc2f808e8785364dfaa8498265
Darya <darya.aniskevich@mail.ru>
2021-02-18 14:02:07 +0300

    daryaA/lesson11

--- lesson11/homeWork.js
+++ lesson11/homeWork.js
@@ -14,12 +14,51 @@
  * Вывести в html внутри div с id = 1
  */
 
+let div1 = document.querySelector('div[id="1"]');
+
+function showUsers() {
+    fetch(`https://jsonplaceholder.typicode.com/users`)
+        .then(response => response.json())
+        .then(users => {
+
+            users.forEach(user => {
+
+                let name = user.name;
+                let phone = user.phone;
+                let userInfo = document.createElement('p');
+                div1.append(userInfo);
+                userInfo.innerHTML = `name: ${name}, phone: ${phone};`
+
+            });
+        })
+}
+
+showUsers();
+
 /**
  * Задание 2
  * Сделать запрос при помощи fetch на адрес https://jsonplaceholder.typicode.com/users/1/todos,
  * вывести todo list пользователя, отобразить текст тудушки и индикатор выполнена она или нет.
  * Вывести в html внутри div с id = 2
  */
+
+let div2 = document.querySelector('div[id="2"]');
+
+function showToDoList() {
+    fetch(`https://jsonplaceholder.typicode.com/users/1/todos`)
+        .then(response => response.json())
+        .then(userToDo => {
+            userToDo.forEach(user => {
+                let userId = user.userId;
+                let toDoName = user.title;
+                let toDoStatus = user.completed;
+                let toDoInfo = document.createElement('p');
+                div2.append(toDoInfo);
+                toDoInfo.innerText = `User Id: ${userId}. Задача ${toDoName} выполнена? ${toDoStatus};`;
+            });
+        })
+}
+showToDoList()
 
 /**
  * Задание 3
@@ -35,6 +74,18 @@
  * Вывести в html внутри div с id = 3
  */
 
+let div3 = document.querySelector('div[id="3"]');
+
+fetch('https://jsonplaceholder.typicode.com/albums/1/photos')
+    .then(response => response.json())
+    .then(photos => {
+        let images = '';
+        for (let photo of photos) {
+            images += `<img src="${photo.url}" width="50" height="50">`
+        };
+        div3.innerHTML = images;
+    })
+
 /**
  * Задание 4
  * Сделать запрос альбомов при помощи fetch на адрес https://jsonplaceholder.typicode.com/albums,
@@ -43,6 +94,8 @@
  * В списке альбомов добавить кнопку Назад, по нажатию на которую переходим обратно на список альбомов.
  */
 
+// :(
+
 /**
  * Задание 5
  *
@@ -50,3 +103,36 @@
  * По нажатию на кнопку сделать POST запрос для добавления нового поста(https://jsonplaceholder.typicode.com/posts).
  * Сделать так, чтобы если в инпутах ничего не введено кнопка должна некликабельна(атрибут disabled).
  */
+
+let input1 = document.createElement('input');
+let input2 = document.createElement('input');
+let button = document.createElement('button');
+let container = document.createElement('div');
+
+container.append(input1, input2, button);
+document.body.append(container);
+
+input1.placeholder = 'Заголовок';
+input2.placeholder = 'Текст поста';
+button.innerText = 'Cделать Post';
+
+button.setAttribute('disabled', '');
+
+container.addEventListener('input', (event) => {
+    if (event.target.tagName !== 'INPUT') {
+        return
+    }
+    if (event.target.value !== '') {
+        button.removeAttribute('disabled')
+    } else {
+        button.setAttribute('disabled', '')
+    }
+});
+
+button.addEventListener('click', () => {
+    let array = [input1.value, input2.value];
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        body: JSON.stringify(array)
+    })
+})
--- lesson11/index.js
+++ lesson11/index.js
@@ -30,26 +30,26 @@
 //Cетевый запросы
 
 function getData(url, callback) {
-  let xhr = new XMLHttpRequest();
-  xhr.open("GET", url);
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", url);
 
-  console.log("Делаем запрос!");
+    console.log("Делаем запрос!");
 
-  xhr.send();
+    xhr.send();
 
-  xhr.onload = function (res) {
-    let result = JSON.parse(res.target.response);
+    xhr.onload = function(res) {
+        let result = JSON.parse(res.target.response);
 
-    callback(result);
-  };
+        callback(result);
+    };
 
-  xhr.onerror = function (error) {
-    console.log(error);
-  };
+    xhr.onerror = function(error) {
+        console.log(error);
+    };
 
-  xhr.onprogress = function (event) {
-    console.log("onprogress", { event });
-  };
+    xhr.onprogress = function(event) {
+        console.log("onprogress", { event });
+    };
 }
 
 let container = document.createElement("div");
@@ -61,51 +61,51 @@ container.style = `
 
 document.body.append(container);
 
-let showUsers = function (usersArr) {
-  usersArr.forEach((item) => {
-    let div = document.createElement("div");
-    let name = document.createElement("p");
-    name.innerText = item?.name;
+let showUsers = function(usersArr) {
+    usersArr.forEach((item) => {
+        let div = document.createElement("div");
+        let name = document.createElement("p");
+        name.innerText = item ? .name;
 
-    let userName = document.createElement("p");
-    userName.innerText = item.username;
+        let userName = document.createElement("p");
+        userName.innerText = item.username;
 
-    let email = document.createElement("p");
-    email.innerText = item.email;
+        let email = document.createElement("p");
+        email.innerText = item.email;
 
-    div.style = `
+        div.style = `
       display: flex;
       flex-direction: column;
       border-bottom: 1px white solid;
     `;
 
-    div.onclick = () => {
-      showCommentsNEW(item.id);
-    };
+        div.onclick = () => {
+            showCommentsNEW(item.id);
+        };
 
-    div.append(name, userName, email);
+        div.append(name, userName, email);
 
-    container.append(div);
-  });
+        container.append(div);
+    });
 };
 
 function showCommentsOLD(userId) {
-  getData("https://jsonplaceholder.typicode.com/posts", function (posts) {
-    let filteredPosts = posts.filter((item) => item.userId === userId);
+    getData("https://jsonplaceholder.typicode.com/posts", function(posts) {
+        let filteredPosts = posts.filter((item) => item.userId === userId);
 
-    getData(
-      "https://jsonplaceholder.typicode.com/comments",
-      function (comments) {
-        const [firstPost] = filteredPosts;
+        getData(
+            "https://jsonplaceholder.typicode.com/comments",
+            function(comments) {
+                const [firstPost] = filteredPosts;
 
-        let filteredComments = comments.filter(
-          (item) => item.postId === firstPost.id
+                let filteredComments = comments.filter(
+                    (item) => item.postId === firstPost.id
+                );
+
+                console.log(filteredComments);
+            }
         );
-
-        console.log(filteredComments);
-      }
-    );
-  });
+    });
 }
 
 // getData("https://jsonplaceholder.typicode.com/users", showUsers);
@@ -113,37 +113,37 @@ function showCommentsOLD(userId) {
 //fetch
 
 function showCommentsNEW(userId) {
-  fetch(`https://jsonplaceholder.typicode.com/users/${userId}/posts`)
-    .then((res) => res.json())
-    .then((posts) => {
-      return fetch(
-        `https://jsonplaceholder.typicode.com/comments?postId=${posts[0].id}`
-      )
+    fetch(`https://jsonplaceholder.typicode.com/users/${userId}/posts`)
         .then((res) => res.json())
-        .then((comments) => {
-          console.log(comments);
+        .then((posts) => {
+            return fetch(
+                    `https://jsonplaceholder.typicode.com/comments?postId=${posts[0].id}`
+                )
+                .then((res) => res.json())
+                .then((comments) => {
+                    console.log(comments);
+                });
         });
-    });
 }
 
 let usersPromise = fetch("https://jsonplaceholder.typicode.com/users")
-  .then((res) => {
-    return res.json();
-  })
-  .then((response) => {
-    showUsers(response);
-  });
+    .then((res) => {
+        return res.json();
+    })
+    .then((response) => {
+        showUsers(response);
+    });
 
 fetch("https://jsonplaceholder.typicode.com/posts", {
-  method: "POST",
-  body: JSON.stringify({
-    title: "FE-42",
-    body: "The best ever!!!",
-    userId: 1,
-  }),
-  headers: {
-    "Content-type": "application/json; charset=UTF-8",
-  },
-})
-  .then((response) => response.json())
-  .then((json) => console.log(json));
+        method: "POST",
+        body: JSON.stringify({
+            title: "FE-42",
+            body: "The best ever!!!",
+            userId: 1,
+        }),
+        headers: {
+            "Content-type": "application/json; charset=UTF-8",
+        },
+    })
+    .then((response) => response.json())
+    .then((json) => console.log(json));
