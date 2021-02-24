43bbbf973c79922c1327a044f5282c4216661bf4
JI3BA <lyovkin_artyom@mail.ru>
2021-02-24 22:44:42 +0300

    lesson13

--- lesson13/index.js
+++ lesson13/index.js
@@ -1,23 +1,33 @@
-import { string } from "./homeWork";
+import { getRandomNumber, IMAGE_SIZE } from "./index1.js";
+import userName from "./index1.js";
+
+// alert(`IMAGE_SIZE: ${IMAGE_SIZE}, random number: ${getRandomNumber()}`);
+// alert(userName);
+
+//this
 
 let user = {
-  name: "Petr",
-  secondName: "Pupkin",
-  email: "email@mail.ru",
-
-  sayHello() {
-    console.log(`Hello, my name is ${this.getFullName()}`);
-  },
-
-  setName(name) {
-    this.name = name;
-  },
-
-  getFullName() {
-    return `${this.name} ${this.secondName}`;
-  },
+  name: "Harry",
+  email: "haha@mail.ru",
 };
 
-user.sayHello();
-user.setName("Vova");
-user.sayHello();
+function showInfo1(message) {
+  console.log(message + " " + this.name);
+}
+
+let showInfo2 = function () {
+  console.log(this);
+};
+
+let showInfo3 = () => {
+  console.log(this);
+};
+
+//bind, call, apply
+
+showInfo1.call(user, "Hello");
+showInfo2.call(user);
+showInfo3.call(user);
+
+let showInfo = showInfo1.bind(user);
+showInfo("Bye bye");
