0bee228614cfebc7de9ccd9e5b1c33435f80e714
[LowlyYl] <[artsiushko.yl@mail.ru]>
2021-02-24 19:18:53 +0300

    AntonA/less13

--- lesson13/homeWork.js
+++ lesson13/homeWork.js
@@ -15,6 +15,9 @@
  * 3. Разделить весь код в разные файлы используя import/export
  */
 
+import sliderWithAJAX from './slider/sliderMain.js'
+sliderWithAJAX.sliderWithAJAX()
+
 /**
  * Задание 2
  * Создать объект работника в него должны быть следующие свойства: имя, фамилия, стаж, email.
@@ -22,11 +25,36 @@
  * будет выводить в alert сообщение 'Привет, ИМЯ! Меня зовут ИМЯ_РАБОТНИКА' (использовать this)
  */
 
+let employeList = {
+    name: 'Diana',
+    surname: 'Lowly',
+    experience: '2 years',
+    email: 'lowly.di@mail.ru',
+
+    sayHello(name) {
+        alert(`Привет, ${name}! Меня зовут ${this.name}`);
+    }
+}
+
+employeList.sayHello("Anton");
+
 /**
  * Задание 3
  * Добавить в объект работника поле количество выполненных деталей. Добавить метод,
  * который будет увеличивать количество деталей на 1. Добавить метод, который будет делать ресет этого количества в 0. (использовать this)
  */
+
+employeList.completedParts = 3;
+
+employeList.completedPartsAdd = function() {
+    return console.log(this.completedParts + 1)
+}
+employeList.completedPartsAdd()
+
+employeList.completedPartsDiv = function() {
+    return console.log(this.completedParts = 0)
+}
+employeList.completedPartsDiv()
 
 /**
  * Задание 4
@@ -34,6 +62,26 @@
  * метод для инкремента(+1) счетчика, метод для декремента(-1) счетчика, метод который будет возвращать (return) значение счетчика.
  * (использовать this)
  */
+
+let count = {
+    countStart: 0,
+
+    increment() {
+        return this.countStart++
+    },
+
+    dincrement() {
+        return this.countStart--
+    },
+
+    returnValue() {
+        return this.countStart
+    }
+}
+count.increment()
+count.increment()
+count.dincrement()
+count.returnValue()
 
 /**
  * Задание 5
@@ -43,3 +91,28 @@
  * Для использования этой функции для каждого из студентов использовать функции привязки контекста(bind или call или apply)
  * и this
  */
+
+let firstStudent = {
+    name: "Diana",
+    age: 21,
+    groupNumber: 'FE42',
+    grades: [8, 10, 6, 9]
+}
+
+let secondStudent = {
+    name: 'Anna',
+    age: 20,
+    groupNumber: 'FE42',
+    grades: [7, 10, 9, 9]
+}
+
+function average() {
+    let sumOfGrades = this.grades.reduce((total, item) => total + item, 0);
+    return sumOfGrades / this.grades.length;
+}
+
+let averageOfFirstStudent = average.bind(firstStudent);
+let averageOfSecondStudent = average.bind(secondStudent);
+
+console.log(averageOfFirstStudent())
+console.log(averageOfSecondStudent())
