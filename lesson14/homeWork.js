281f41f9c74e468a24c156c6ea900215e31c8513
Yauheni Bandyukevich <zhenya.bandyukevich@mail.ru>
2021-02-27 15:19:35 +0300

    hw done

--- lesson14/homeWork.js
+++ lesson14/homeWork.js
@@ -7,6 +7,39 @@
  * Зарплата - это произведение (умножение) ставки rate на количество отработанных дней days.
  */
 
+function MyWorker(name, secondName, rate, days) {
+  this.name = name;
+  this.secondName = secondName;
+  this.rate = rate;
+  this.days = days;
+}
+MyWorker.prototype.getSalary = function () {
+  return this.rate * this.days;
+};
+
+MyWorker.prototype.getName = function () {
+  return this.name;
+};
+MyWorker.prototype.getSecondName = function () {
+  return this.secondName;
+};
+MyWorker.prototype.getRate = function () {
+  return this.rate;
+};
+MyWorker.prototype.getDays = function () {
+  return this.days;
+};
+MyWorker.prototype.setRate = function (rate) {
+  this.rate = rate;
+};
+MyWorker.prototype.setDays = function (days) {
+  this.days = days;
+};
+
+const worker1 = new MyWorker('Petr', 'Pupkin', 50, 30);
+console.log(worker1);
+console.log('worker1.getSalary: ', worker1.getSalary());
+
 /**
  * Задание 2
  * Модифицируйте функцию конструктор Worker из предыдущей задачи следующим образом:
@@ -15,11 +48,56 @@
  * И также методы setRate, setDays, которые будут устанавливать новые значения для соответственно свойств rate и days.
  */
 
+const worker2 = new MyWorker('Ivan', 'Ivanov', 10, 10);
+console.log(worker2);
+worker2.setDays(25);
+console.log(worker2);
+
 /**
  * Задание 3
  * Сделать задание 1-2 с использованием классов.
  *
  */
+
+class WorkerClass {
+  constructor(name, secondName, rate, days) {
+    this.name = name;
+    this.secondName = secondName;
+    this.rate = rate;
+    this.days = days;
+  }
+
+  getSalary() {
+    return this.rate * this.days;
+  }
+  getName() {
+    return this.name;
+  }
+  getSecond() {
+    return this.secondName;
+  }
+
+  getRate() {
+    return this.rate;
+  }
+
+  getDays() {
+    return this.days;
+  }
+
+  setRate(rate) {
+    this.rate = rate;
+  }
+  setDays(days) {
+    this.days = days;
+  }
+}
+
+const worker3 = new WorkerClass('Pasha', 'Sidorov', 100, 10);
+console.log(worker3);
+worker3.setDays(30);
+console.log(worker3);
+console.log(worker3.getSalary());
 
 /**
  * Задание 4
@@ -30,6 +108,25 @@
  * и метод ucWords,
  * который принимает строку и делает заглавной первую букву каждого слова этой строки.
  */
+function MyString() {
+  this.reverse = function (str) {
+    return str.split('').reverse().join('');
+  };
+  this.ucFirst = function (str) {
+    return str[0].toUpperCase() + str.slice(1);
+  };
+  this.ucWords = function (str) {
+    return str
+      .split(' ')
+      .map((word) => word[0].toUpperCase() + word.slice(1))
+      .join(' ');
+  };
+}
+
+const string = new MyString();
+console.log(string.reverse('hello'));
+console.log(string.ucFirst('teststring'));
+console.log(string.ucWords('test string'));
 
 /**
  * Задание 5
@@ -37,11 +134,44 @@
  *
  */
 
+class MyStringClass {
+  reverse(str) {
+    return str.split('').reverse().join('');
+  }
+  ucFirst(str) {
+    return str[0].toUpperCase() + str.slice(1);
+  }
+  ucWords(str) {
+    return str
+      .split(' ')
+      .map((word) => word[0].toUpperCase() + word.slice(1))
+      .join(' ');
+  }
+}
+
+const string2 = new MyStringClass();
+console.log(string2.ucWords('two words'));
+
 /**
  * Задание 6
  * Реализуйте класс User, который будет иметь следующие свойства: имя, фамилия, email; следующие методы:
  * getFullName, getEmail.
  */
+class User {
+  constructor(name, surname, email) {
+    this.name = name;
+    this.surname = surname;
+    this.email = email;
+  }
+
+  getFullName() {
+    return `${this.name} ${this.surname}`;
+  }
+
+  getEmail() {
+    return this.email;
+  }
+}
 
 /**
  * Задание 7
@@ -52,3 +182,23 @@
  * который будет выводить текущий курс студента (от 1 до 5). Курс вычисляется так: нужно от текущего года отнять год
  * поступления в вуз. Текущий год получить программно, используя объект Date (https://learn.javascript.ru/datetime).
  */
+
+class Student extends User {
+  constructor(name, surname, year) {
+    super(name, surname);
+    this.year = year;
+  }
+
+  getFullName() {
+    return super.getFullName();
+  }
+
+  getCourse() {
+    const date = new Date();
+    return `вы на ${date.getFullYear() - this.year} курсе`;
+  }
+}
+
+const student = new Student('Anya', 'Guseva', 2019);
+console.log(student.getCourse());
+console.log(student.getFullName());
