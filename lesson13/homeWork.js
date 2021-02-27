7cbc11a674da3546c007728f60bf6e590a854d1a
JI3BA <lyovkin_artyom@mail.ru>
2021-02-28 02:56:51 +0300

    homework lesson14

--- lesson13/homeWork.js
+++ lesson13/homeWork.js
@@ -5,18 +5,6 @@
  * После выполнения задания делаете пулл реквест и сообщаете о том, что задание выполнено:)
  * В случае неполодок с гитом, присылаете папку с проектом мне в личные сообщения
  * Код решения должен быть сразу же после самого задания
-<<<<<<< HEAD
- */
-
-/**
- * Домашнее задание к занятию 13
- * Каждому нужно создать новую ветку от ветки master. Название ветки должно содержать имя + первая буква фамилии + /lesson13.
- * Например: dmitryR/lesson13. Выполнение задания производится в свое ветке и в этом файле.
- * После выполнения задания делаете пулл реквест и сообщаете о том, что задание выполнено:)
- * В случае неполодок с гитом, присылаете папку с проектом мне в личные сообщения
- * Код решения должен быть сразу же после самого задания
-=======
->>>>>>> c31149d5994b8610c95fc72d173311518e3db3d4
  */
 
 /**
@@ -33,36 +21,6 @@ import {expSlider} from './slider.js';
  * Создать объект работника в него должны быть следующие свойства: имя, фамилия, стаж, email.
  * Плюс у него должны быть методы: sayHello - функция, которая принимает имя и
  * будет выводить в alert сообщение 'Привет, ИМЯ! Меня зовут ИМЯ_РАБОТНИКА' (использовать this)
-<<<<<<< HEAD
- */
-// let UserWork = {
-//     name: 'Maks',
-//     subname: 'Petrovich',
-//     experience: 'Two years',
-//     email: 'Petrovich@ggggg.com',
-//     sayHello(name){
-//         alert(`Привет, ${name}! Меня зовут ${this.name}`)
-//     }
-// }
-// UserWork.sayHello('Maks');  
-
-/**
- * Задание 3
- * Добавить в объект работника поле количество выполненных деталей. Добавить метод,
- * который будет увеличивать количество деталей на 1. Добавить метод, который будет делать ресет этого количества в 0. (использовать this)
- */
-// UserWork.CompletedDetails = 1;
-
-// UserWork.UpdateDetails = function(){
-//     alert(`Теперь количество выполненых деталей ${this.CompletedDetails + 1}`);
-// }
-// UserWork.UpdateDetails()
-
-// UserWork.ResetDetails = function() {
-//     alert(`В итоге количество выполненых деталей ${this.CompletedDetails = 0}`);
-// }
-// UserWork.ResetDetails()
-=======
  */
 
 /**
@@ -71,33 +29,12 @@ import {expSlider} from './slider.js';
  * который будет увеличивать количество деталей на 1. Добавить метод, который будет делать ресет этого количества в 0. (использовать this)
  */
 
->>>>>>> c31149d5994b8610c95fc72d173311518e3db3d4
 /**
  * Задание 4
  * Создать объект счетчика, который будет иметь поле count - значение счетчика. Объект будет иметь следующие методы:
  * метод для инкремента(+1) счетчика, метод для декремента(-1) счетчика, метод который будет возвращать (return) значение счетчика.
  * (использовать this)
  */
-<<<<<<< HEAD
-// let Counter = {
-//     count: 0,
-//     CountIncrement(){
-//         this.count++
-//     },
-
-//     CountDecrement(){
-//          this.count--
-//     },
-//     CountValue(){
-//         return console.log(this.count);
-//     }
-// }
-// Counter.CountIncrement()
-// Counter.CountDecrement()
-// Counter.CountValue()
-=======
-
->>>>>>> c31149d5994b8610c95fc72d173311518e3db3d4
 /**
  * Задание 5
  * Создать несколько объектов student c полями имя, возраст, номер группы, массив оценок.
@@ -106,38 +43,6 @@ import {expSlider} from './slider.js';
  * Для использования этой функции для каждого из студентов использовать функции привязки контекста(bind или call или apply)
  * и this
  */
-<<<<<<< HEAD
-// let StudentOne = {
-//     name: 'Vasya',
-//     age: 21,
-//     numberGroup: '5',
-//     rating: [7, 5, 6, 9],
-// }
-// let StudentTwo = {
-//     name: 'Pete',
-//     age: 18,
-//     numberGroup: '1',
-//     rating: [9, 8, 7, 9],
-// }
-// let StudentThree = {
-//     name: 'Andry',
-//     age: 25,
-//     numberGroup: '5',
-//     rating: [4, 4, 6, 5],
-// }
-// function CalcRating() {
-//     let ratingSum = this.rating.reduce((sum,curr) => sum + curr, 0);
-//     let ratingLen = this.rating.length;
-//     return console.log(ratingSum / ratingLen);
-// }
-// // let resultStudents = CalcRating.bind(StudentOne)
-// // resultStudents()
-
-
-// CalcRating.call(StudentOne);
-// CalcRating.call(StudentTwo);
-// CalcRating.call(StudentThree);
-=======
 let stud1 = {
   name: "Archi",
   age: 20,
@@ -168,4 +73,3 @@ function getAverageMark() {
 
 console.log(getAverageMark.apply(stud1));
 console.log(stud1);
->>>>>>> c31149d5994b8610c95fc72d173311518e3db3d4
--- lesson14/homeWork.js
+++ lesson14/homeWork.js
@@ -6,7 +6,19 @@
  * который будет выводить зарплату работника.
  * Зарплата - это произведение (умножение) ставки rate на количество отработанных дней days.
  */
+// function onlyWorker(name, secondName, rate, days) {
+//     this.name = name;
+//     this.secondName = secondName;
+//     this.rate = rate;
+//     this.days = days;
+// }
 
+// onlyWorker.prototype.getSalary = function() {
+//      console.log(this.rate * this.days)
+// }
+// let worker = new onlyWorker('Artyom','Kraken', 500, 30)
+// worker.getSalary()
+// console.log(worker)
 /**
  * Задание 2
  * Модифицируйте функцию конструктор Worker из предыдущей задачи следующим образом:
@@ -14,12 +26,85 @@
  * каждый из которых будет возвращать соответствующее поле в объекте.
  * И также методы setRate, setDays, которые будут устанавливать новые значения для соответственно свойств rate и days.
  */
+// onlyWorker.prototype.getName = function() {
+//     return this.name;
+// }
+
+// onlyWorker.prototype.getSecondName = function() {
+//     return this.secondName;
+// }
+
+// onlyWorker.prototype.getRate = function() {
+//     return this.rate;
+// }
+
+// onlyWorker.prototype.getDays = function() {
+//     return this.days;
+// }
+
+// onlyWorker.prototype.setRate = function(rate) {
+//     this.rate = rate;
+// }
+// onlyWorker.prototype.setDays = function(days) {
+//     this.days = days;
+// }
+// worker.setRate(20);
+// worker.setDays(20);
+// worker.getSalary();
+// console.log(worker)
 
 /**
  * Задание 3
  * Сделать задание 1-2 с использованием классов.
  *
  */
+// class OnlyNewWorker {
+//     constructor(name, secondName, rate, days){
+//         this.name = name;
+//         this.secondName = secondName;
+//         this.rate = rate;
+//         this.days = days;
+//     }
+
+//     get getSalary(){
+//         return this.rate * this.days;
+//     }
+
+//     get getName(){
+//         return this.name;
+//     }
+
+//     get getSecondName(){
+//         return this.secondName;
+//     }
+
+//     get getRate(){
+//         return this.rate;
+//     }
+
+//     get getDays(){
+//         return this.days;
+//     }
+
+//     setRate(rate){
+//         this.rate = rate;
+//     }
+
+//     setDays(days){
+//         this.days = days;
+//     }
+// }
+// let secondWorker = new OnlyNewWorker('Pete','Sokol', 50, 10);
+
+// console.log(secondWorker);
+// console.log(secondWorker.getSalary);
+
+// secondWorker.setRate(100);
+// secondWorker.setDays(20);
+
+// console.log(secondWorker);
+// console.log(secondWorker.getSalary);
+
 
 /**
  * Задание 4
@@ -30,19 +115,88 @@
  * и метод ucWords,
  * который принимает строку и делает заглавной первую букву каждого слова этой строки.
  */
+// function myString(str) {
+//     this.str = str;
+// }
 
+// myString.prototype.reverse = function() {
+//     return console.log(this.str.split('').reverse().join(''));
+// }
+
+// myString.prototype.ucFirst = function() {
+//     let arr = this.str.split('');
+//     let fullArr = arr[0].toUpperCase() + arr.slice(1).join('').toLowerCase();
+//     return console.log(fullArr);
+// }
+
+// myString.prototype.ucWords = function() {
+//     let array = this.str.split(' ');
+//     let arrUcWords = array.map(item => item[0].toUpperCase() + item.slice(1).toLowerCase()).join(' ')
+//     return console.log(arrUcWords);
+// }
+
+// let str = new myString('hello mY fRiend');
+// str.reverse();
+// str.ucFirst();
+// str.ucWords();
+// console.log(str)
+    
 /**
  * Задание 5
  * Сделать задание 4 с использованием классов.
  *
  */
+// class MyString {
+//     constructor(str){
+//         this.str = str;
+//     }
+
+//     reverse(){
+//         return console.log(this.str.split('').reverse().join(''));
+//     }
+
+//     ucFirst(){
+//         let arr = this.str.split('');
+//         let fullArr = arr[0].toUpperCase() + arr.slice(1).join('').toLowerCase();
+//         return console.log(fullArr);
+//     }
+
+//     ucWords(){
+//         let array = this.str.split(' ');
+//         let arrUcWords = array.map(item => item[0].toUpperCase() + item.slice(1).toLowerCase()).join(' ')
+//         return console.log(arrUcWords);
+//     }
+// }
+
+// let onlyMyString = new MyString('reLease the kraKen');
+// onlyMyString.reverse();
+// onlyMyString.ucFirst();
+// onlyMyString.ucWords();
 
 /**
  * Задание 6
  * Реализуйте класс User, который будет иметь следующие свойства: имя, фамилия, email; следующие методы:
  * getFullName, getEmail.
  */
+class User {
+    constructor(name ,secondname, email){
+        this.name = name;
+        this.secondname = secondname;
+        this.email = email;
+    }
 
+    getFullName(){
+        return console.log(this.name +' '+ this.secondname);
+    }
+
+    getEmail(){
+        return console.log(this.email);
+    }
+}
+
+let newUser = new User('Artyom','Lyovkin','JI3BA@mdsa.com');
+newUser.getFullName()
+newUser.getEmail()
 /**
  * Задание 7
  * Реализуйте класс Student, который будет наследовать класс User.
@@ -52,58 +206,31 @@
  * который будет выводить текущий курс студента (от 1 до 5). Курс вычисляется так: нужно от текущего года отнять год
  * поступления в вуз. Текущий год получить программно, используя объект Date (https://learn.javascript.ru/datetime).
  */
-<<<<<<< HEAD
-
-class Student extends User {
-  constructor(name, surname, email, year) {
-    super(name, surname, email);
-
-    this.year = year;
-  }
-
-  getCourse() {
-    let date = new Date();
-    let currentYear = date.getFullYear();
-
-    let diff = currentYear - this.year;
-
-    if (diff === 0) {
-      return 1;
+class Student extends User{
+    constructor(name, secondname, year){
+        super(name,secondname)
+        this.year = year;
     }
 
-    if (diff >= 1 && diff <= 5) {
-      return diff;
+    getFullName(){
+        super.getFullName()
     }
 
-    alert("Error");
-  }
+    getCourse(){
+        let date = new Date(this.year)
+        let dateNow = new Date();
+        let newDate = dateNow.getFullYear()
+        let Now = newDate - date;
+        if(Now === 0){
+            alert('Вы ещё не поступили')
+        }else if(Now > 5){
+            alert('Вы уже закончили')
+        }else{
+            alert(`Cейчас ${Now} курс`)
+        }
+    }
 }
 
-/**
- * Задание 0
- * Реализуйте функцию конструктор Worker (Работник), который будет создавать объект и иметь следующие свойства: name (имя),
- * secondName (фамилия),
- * rate (ставка за день работы), days (количество отработанных дней). Также класс должен иметь метод getSalary(),
- * который будет выводить зарплату работника.
- * Зарплата - это произведение (умножение) ставки rate на количество отработанных дней days.
- */
-
-/**
- * Задание 1
- * Модифицируйте функцию конструктор Worker из предыдущей задачи следующим образом:
- * У каждого объекта класса Worker должны быть доступны методы getName, getSecondName, getRate, getDays,
- * каждый из которых будет возвращать соответствующее поле в объекте.
- * И также методы setRate, setDays, которые будут устанавливать новые значения для соответственно свойств rate и days.
- */
-
-/**
- * Задание 2
- * Реализуйте функцию конструктор MyString, объект которого будет иметь следующие методы: метод reverse(),
- * который параметром принимает строку, а возвращает ее в перевернутом виде,
- * метод ucFirst(),
- * который параметром принимает строку, а возвращает эту же строку, сделав ее первую букву заглавной
- * и метод ucWords,
- * который принимает строку и делает заглавной первую букву каждого слова этой строки.
- */
-=======
->>>>>>> c31149d5994b8610c95fc72d173311518e3db3d4
+let student = new Student('Andry', 'Petrovich', 2017);
+student.getFullName();
+student.getCourse();
