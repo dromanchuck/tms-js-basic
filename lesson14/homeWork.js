da6e74311ae147b5332a8cdef7826c9465c94ded
[LowlyYl] <[artsiushko.yl@mail.ru]>
2021-02-27 03:36:31 +0300

    AntonA/less14

--- lesson14/homeWork.js
+++ lesson14/homeWork.js
@@ -7,6 +7,17 @@
  * Зарплата - это произведение (умножение) ставки rate на количество отработанных дней days.
  */
 
+function Worker(name, secondName, rate, days) {
+    this.name = name;
+    this.secondName = secondName;
+    this.rate = rate;
+    this.days = days;
+  }
+
+Worker.prototype.getSalary = function() {
+    console.log(`salary: ${this.rate * this.days}`)
+}
+
 /**
  * Задание 2
  * Модифицируйте функцию конструктор Worker из предыдущей задачи следующим образом:
@@ -15,11 +26,49 @@
  * И также методы setRate, setDays, которые будут устанавливать новые значения для соответственно свойств rate и days.
  */
 
+Worker.prototype.getName = function() {return this.name}
+Worker.prototype.getSecondName = function() {return this.secondName}
+Worker.prototype.getRate = function() {return this.rate}
+Worker.prototype.getDays = function() {return this.days}
+Worker.prototype.setRate = function(rate) {this.rate = rate}
+Worker.prototype.setDays = function(days) {this.days = days}    
+
 /**
  * Задание 3
  * Сделать задание 1-2 с использованием классов.
  *
  */
+
+class NewWorker {
+    constructor(name, secondName, rate, days) {
+        this.name = name;
+        this.secondName = secondName;
+        this.rate = rate;
+        this.days = days; 
+    }
+
+    getSalary() {
+        console.log(`salary: ${this.rate * this.days}`)
+    }
+    getName() {
+        return this.name
+    }
+    getSecondName() {
+        return this.secondName
+    }
+    getRate() 
+    {return this.rate
+    }
+    getDays() {
+        return this.days
+    }
+    setRate(rate) {
+        this.days = rate
+    }
+    setDays(days) {
+        this.days = days
+    }
+}
 
 /**
  * Задание 4
@@ -31,17 +80,79 @@
  * который принимает строку и делает заглавной первую букву каждого слова этой строки.
  */
 
+ function MyString(str) {
+    this.str = str;
+ }
+
+ MyString.prototype.reverse = function() {
+    return this.str.toLowerCase().split('').reverse().join('');
+ }
+
+ MyString.prototype.ucFirst = function() {
+    let strArr = this.str.toLowerCase().split('');
+    return strArr[0].toUpperCase() + strArr.slice(1).join('');
+ }
+
+ MyString.prototype.ucWords = function() {
+    let strArr = this.str.toLowerCase().split(' ');
+    let newArr = strArr.map((item) => {
+        let words = item.split('');
+        return words[0].toUpperCase() + words.slice(1).join('');
+    })
+    return newArr.join(' ')
+ }
+
 /**
  * Задание 5
  * Сделать задание 4 с использованием классов.
  *
  */
 
+ class NewMyString {
+    constructor(str) {
+        this.str = str;
+    }
+
+    reverse() {
+        return this.str.toLowerCase().split('').reverse().join('');
+    }
+
+    ucFirst() {
+        let strArr = this.str.toLowerCase().split('');
+        return strArr[0].toUpperCase() + strArr.slice(1).join('');
+    }
+
+    ucWords() {
+        let strArr = this.str.toLowerCase().split(' ');
+        let newArr = strArr.map((item) => {
+        let words = item.split('');
+        return words[0].toUpperCase() + words.slice(1).join('');
+        })
+        return newArr.join(' ')
+    }
+ } 
+
 /**
  * Задание 6
  * Реализуйте класс User, который будет иметь следующие свойства: имя, фамилия, email; следующие методы:
  * getFullName, getEmail.
  */
+
+class User {
+    constructor(name, secondName, email) {
+        this.name = name;
+        this.secondName = secondName;
+        this.email = email;
+    }
+
+    getFullName() {
+        return `${this.name} ${this.secondName}`
+    }
+
+    getEmail() {
+        return this.email
+    }
+}
 
 /**
  * Задание 7
@@ -52,3 +163,19 @@
  * который будет выводить текущий курс студента (от 1 до 5). Курс вычисляется так: нужно от текущего года отнять год
  * поступления в вуз. Текущий год получить программно, используя объект Date (https://learn.javascript.ru/datetime).
  */
+
+ class Student extends User {
+    constructor(name, secondName, year) {
+        super(name, secondName);
+        this.year = year;
+    }
+
+    getFullName() {
+        return super.getFullName()
+    }
+
+    getCourse() {
+        let date = new Date();
+        return `вы на ${date.getFullYear() - this.year} курсе`
+    }
+ }
