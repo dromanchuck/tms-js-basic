ca5bb62fc4b1901b37f24b4893634606e5105222
Ilya <ilya.naumenko53@gmail.com>
2021-02-28 00:42:23 +0300

    IlyaN/Lesson14

--- lesson14/homeWork.js
+++ lesson14/homeWork.js
@@ -7,6 +7,55 @@
  * Зарплата - это произведение (умножение) ставки rate на количество отработанных дней days.
  */
 
+function Worker(name, secondName, rate, days) {
+  this.name = name
+  this.secondName = secondName
+  this.rate = rate
+  this.days = days
+
+  this.getSalary = function () {
+    return this.rate * this.days
+  }
+
+  this.getName = function () {
+    return this.name
+  }
+
+  this.getSecondName = function () {
+    return this.secondName
+  }
+
+  this.getRate = function () {
+    return this.rate
+  }
+
+  this.getDays = function () {
+    return this.days
+  }
+
+  this.setRate = function (newRate) {
+    this.rate = newRate
+    return this.rate
+  }
+
+  this.setDays = function (newDays) {
+    this.days = newDays
+    return this.days
+  }
+}
+
+let worker = new Worker('Archibald', 'Ramirez', 1000, 30)
+
+console.log(worker)
+console.log(worker.getSalary())
+console.log(worker.getName())
+console.log(worker.getSecondName())
+console.log(worker.getRate())
+console.log(worker.getDays())
+console.log(worker.setRate(2000))
+console.log(worker.setDays(31))
+console.log(worker)
+
 /**
  * Задание 2
  * Модифицируйте функцию конструктор Worker из предыдущей задачи следующим образом:
@@ -21,6 +70,53 @@
  *
  */
 
+class superWorker {
+  constructor(name, secondName, rate, days) {
+    this.name = name
+    this.secondName = secondName
+    this.rate = rate
+    this.days = days
+  }
+  get salary() {
+    return this.rate * this.days
+  }
+
+  getName() {
+    return this.name
+  }
+
+  getSecondName() {
+    return this.secondName
+  }
+
+  get Rate() {
+    return this.rate
+  }
+
+  get Days() {
+    return this.days
+  }
+
+  set Rate(newRate) {
+    this.rate = newRate
+  }
+
+  set Days(newDays) {
+    this.days = newDays
+  }
+}
+let mySuperWorker = new superWorker('Inna', 'Poznyak', 12, 13)
+
+console.log(mySuperWorker)
+console.log(mySuperWorker.salary)
+console.log(mySuperWorker.name)
+console.log(mySuperWorker.secondName)
+console.log(mySuperWorker.Rate)
+console.log(mySuperWorker.Days)
+console.log((mySuperWorker.Rate = 1000))
+console.log((mySuperWorker.Days = 10))
+console.log(mySuperWorker)
+
 /**
  * Задание 4
  * Реализуйте функцию конструктор MyString, объект которого будет иметь следующие методы: метод reverse(),
@@ -31,17 +127,87 @@
  * который принимает строку и делает заглавной первую букву каждого слова этой строки.
  */
 
+function MyString(string) {
+  this.string = string
+
+  this.reverse = function (str) {
+    return str.split('').reverse().join('')
+  }
+
+  this.ucFirst = function (str) {
+    if (!str) return 'Введи что-то!'
+    return str[0].toUpperCase() + str.slice(1)
+  }
+
+  this.ucWords = function (str) {
+    return str
+      .split(' ')
+      .map((elem) => elem[0].toUpperCase() + elem.slice(1))
+      .join(' ')
+  }
+}
+
+let superString = new MyString()
+
+console.log(superString.reverse('Hello'))
+console.log(superString.ucFirst('hello'))
+console.log(superString.ucWords('hello hello hello'))
+
 /**
  * Задание 5
  * Сделать задание 4 с использованием классов.
  *
  */
 
+class myString {
+  reverse(str) {
+    return str.split('').reverse().join('')
+  }
+
+  ucFirst(str) {
+    if (!str) return 'Введи что-то!'
+    return str[0].toUpperCase() + str.slice(1)
+  }
+
+  ucWords(str) {
+    return str
+      .split(' ')
+      .map((elem) => elem[0].toUpperCase() + elem.slice(1))
+      .join(' ')
+  }
+}
+
+let mySuperString = new myString()
+
+console.log(mySuperString.reverse('Hello'))
+console.log(mySuperString.ucFirst('hello'))
+console.log(mySuperString.ucWords('hello hello hello'))
 /**
  * Задание 6
  * Реализуйте класс User, который будет иметь следующие свойства: имя, фамилия, email; следующие методы:
  * getFullName, getEmail.
  */
+
+class User {
+  constructor(name, surname, email) {
+    this.name = name
+    this.surname = surname
+    this.email = email
+  }
+
+  get FullName() {
+    return `${this.name} ${this.surname}`
+  }
+
+  get Email() {
+    return this.email
+  }
+}
+
+let SuperUser = new User('Usha', 'Milas', 'mike@gmail.com')
+
+console.log(SuperUser.FullName)
+console.log(SuperUser.Email)
 
 /**
  * Задание 7
@@ -52,3 +218,18 @@
  * который будет выводить текущий курс студента (от 1 до 5). Курс вычисляется так: нужно от текущего года отнять год
  * поступления в вуз. Текущий год получить программно, используя объект Date (https://learn.javascript.ru/datetime).
  */
+
+class Student extends User {
+  constructor(name, surname, year) {
+    super(name, surname)
+    this.year = year
+  }
+
+  get Course() {
+    return new Date().getFullYear() - this.year
+  }
+}
+
+let std = new Student('name', 'surname', 2018)
+
+console.log(std.Course)
