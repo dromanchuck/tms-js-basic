ac618964e0797b1d25c457c983001baf129e8572
Ilya <ilya.naumenko53@gmail.com>
2021-03-04 19:30:17 +0300

    someFix

--- lesson08/homeWork.js
+++ lesson08/homeWork.js
@@ -23,36 +23,27 @@
  *
  */
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> master
 /**
  * Задание №2
  * Получить кнопку с текстом click. Сделать так, чтобы по нажатию на кнопку вызывался alert('Hello!!!');
  *
  */
-<<<<<<< HEAD
-=======
-let div1 = document.getElementById("unordered_list");
-let div2 = document.getElementsByClassName("ordered_list")[1];
-let div3 = document.querySelector("#unordered_list");
+let div1 = document.getElementById('unordered_list')
+let div2 = document.getElementsByClassName('ordered_list')[1]
+let div3 = document.querySelector('#unordered_list')
 
 /**
  * Задание №2
  * Получить кнопку с текстом click. Сделать так, чтобы по нажатию на кнопку вызывался alert('Hello!!!');
  */
-let buttons = document.getElementsByTagName("button");
-let buttonsArr = [...buttons];
+let buttons = document.getElementsByTagName('button')
+let buttonsArr = [...buttons]
 
-let buttonClick = buttonsArr.find((item) => item.innerText === "click");
+let buttonClick = buttonsArr.find((item) => item.innerText === 'click')
 
 buttonClick.onclick = function () {
-  alert("Hello!!!");
-};
->>>>>>> 1b82d5514e453e30d95353ad23749ef94731177f
-=======
->>>>>>> master
+  alert('Hello!!!')
+}
 
 /**
  * Задание №3
@@ -62,32 +53,20 @@ buttonClick.onclick = function () {
  *
  */
 
-=======
- */
-
 for (let button of buttons) {
   button.style = `
     font-family: arial;
     font-size: 20px;
-   `;
+   `
 
-  button.innerText = button.innerText.toUpperCase();
+  button.innerText = button.innerText.toUpperCase()
 }
 
->>>>>>> 1b82d5514e453e30d95353ad23749ef94731177f
-=======
- *
- */
-
->>>>>>> master
 /**
  * Задание №4
  * Получить все элементы li c классом list_item. Изменить стиль(использовать inline стили и добавление класса).
  * Сделать все буквы маленькими у каждого текстого узла li. Добавить каждому li кнопку c текстом ok.
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> master
+
  *
  */
 
@@ -95,30 +74,27 @@ for (let button of buttons) {
  * Задание 5
  * Создать элементы input и button добавить их в верстку файла index.html. Сделать так. чтобы по нажатию на
  * кнопку текст преобразовывался в кэмэл кейс.
-<<<<<<< HEAD
-=======
+
  */
 
-let liCollection = document.querySelectorAll(".list_item");
+let liCollection = document.querySelectorAll('.list_item')
 
 for (let li of liCollection) {
-  let button = document.createElement("button");
-  let text = document.createTextNode("ok");
-  button.append(text);
-  li.append(button);
+  let button = document.createElement('button')
+  let text = document.createTextNode('ok')
+  button.append(text)
+  li.append(button)
 
-  li.style.color = "white";
+  li.style.color = 'white'
 
-  li.classList.add("background");
+  li.classList.add('background')
 }
 
 /**
  * Задание 5
  * Создать элементы input и button добавить их в верстку файла index.html. Сделать так. чтобы по нажатию на
  * кнопку, текст инпута преобразовывался в кэмэл кейс.
->>>>>>> 1b82d5514e453e30d95353ad23749ef94731177f
-=======
->>>>>>> master
+
  * Например:
  * 'Введенный текст' -> 'введенныйТекст'
  * 'CaMel CaSe -> camelCase
@@ -126,45 +102,39 @@ for (let li of liCollection) {
  *
  */
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
 function capitalize(str) {
   return str
-    .split("")
+    .split('')
     .map((value, index) =>
       index === 0 ? value.toUpperCase() : value.toLowerCase()
     )
-    .join("");
+    .join('')
 }
 
 function toCamelCase(str) {
   return str
     .toLowerCase()
-    .split(" ")
+    .split(' ')
     .reduce((acc, value, index) => {
-      return index === 0 ? acc + value : acc + capitalize(value);
-    }, "");
+      return index === 0 ? acc + value : acc + capitalize(value)
+    }, '')
 }
 
-let input = document.createElement("input");
-let fragment = new DocumentFragment();
+let input = document.createElement('input')
+let fragment = new DocumentFragment()
 
-let button = document.createElement("button");
-button.innerText = "To Camel case";
+let button = document.createElement('button')
+button.innerText = 'To Camel case'
 
 button.onclick = function () {
-  let newValue = toCamelCase(input.value);
+  let newValue = toCamelCase(input.value)
 
-  input.value = newValue;
-};
+  input.value = newValue
+}
 
-fragment.append(input, button);
-document.body.append(fragment);
+fragment.append(input, button)
+document.body.append(fragment)
 
->>>>>>> 1b82d5514e453e30d95353ad23749ef94731177f
-=======
->>>>>>> master
 /**
  * Задание 6
  * Создать элементы 2 input'a и button добавить их в верстку файла index.html. Сделать так,
@@ -176,32 +146,27 @@ document.body.append(fragment);
  * Инпут2: 'Hello'
  */
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> master
 /**
  * Задание 7
  * Написать код, который будет каждую минуту будет менять цвет у страницы. Цвет должен генерироваться рандомно.
  *
  */
-<<<<<<< HEAD
-=======
-let input1 = document.createElement("input");
-let input2 = document.createElement("input");
 
-let btn = document.createElement("button");
-btn.innerText = "Поменять";
+let input1 = document.createElement('input')
+let input2 = document.createElement('input')
+
+let btn = document.createElement('button')
+btn.innerText = 'Поменять'
 btn.onclick = function () {
-  let temp = input1.value;
-  input1.value = input2.value;
-  input2.value = temp;
-};
+  let temp = input1.value
+  input1.value = input2.value
+  input2.value = temp
+}
 
-let div = document.createElement("div");
-div.append(input1, btn, input2);
+let div = document.createElement('div')
+div.append(input1, btn, input2)
 
-document.body.append(div);
+document.body.append(div)
 
 /**
  * Задание 7
@@ -209,14 +174,11 @@ document.body.append(div);
  */
 
 function generateColor() {
-  return "#" + Math.floor(Math.random() * 16777215).toString(16);
+  return '#' + Math.floor(Math.random() * 16777215).toString(16)
 }
 
 setInterval(() => {
-  let color = generateColor();
+  let color = generateColor()
 
-  document.body.style.background = color;
-}, 1000);
->>>>>>> 1b82d5514e453e30d95353ad23749ef94731177f
-=======
->>>>>>> master
+  document.body.style.background = color
+}, 1000)
