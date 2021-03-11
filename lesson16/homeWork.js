2fb4bcddfdb07528b1bc9fa3997cc756b61e0fa2
Darya <darya.aniskevich@mail.ru>
2021-03-11 16:24:32 +0300

    daryaA/lesson16

--- lesson16/homeWork.js
+++ lesson16/homeWork.js
@@ -20,3 +20,129 @@
  * По нажатию на кнопки Следующий месяц и Предыдущий месяц должна производится навигация по месяцам.
  * Сегодняшний день должен быть закрашеным. День 8е марта тоже должен быть закращеным каким-нибудь другим цветом.
  */
+
+let prevButton = document.querySelector('#prev');
+let nextButton = document.querySelector('#next');
+let calendar = document.querySelector('#calendar');
+let currentMonth = document.querySelector('#date');
+
+let getCurrentMonth = new Date().getMonth();
+let getCurrentYear = new Date().getFullYear();
+
+function makeCalendar(year, month) {
+
+    let lastDayOfMonth = new Date(year,month+1,0).getDate();
+    let D = new Date(year,month,lastDayOfMonth);
+    let dayOfWeekLastDayOfMonth = new Date(D.getFullYear(),D.getMonth(),lastDayOfMonth).getDay();
+    let dayOfWeekFirstDayOfMonth = new Date(D.getFullYear(),D.getMonth(),1).getDay();
+    let monthArray = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'];
+    let publicHolidays = [
+        {month: 'Январь', day: '1'},
+        {month: 'Январь', day: '7'},
+        {month: 'Март', day: '8'},
+        {month: 'Май', day: '1'},
+        {month: 'Май', day: '9'},
+        {month: 'Июль', day: '3'},
+        {month: 'Ноябрь', day: '7'},
+        {month: 'Декабрь', day: '25'}
+      ];
+
+    if (dayOfWeekFirstDayOfMonth !=0) {
+        for(let i=1; i<dayOfWeekFirstDayOfMonth; i++) {
+            let day = document.createElement('div');
+            day.classList.add('day');
+            calendar.append(day) ; 
+        }   
+    } else {
+        for(let i=0; i<6; i++) {
+            let day = document.createElement('div');
+            day.classList.add('day');
+            calendar.append(day); 
+        }
+    }
+
+    for(let i = 1; i <= lastDayOfMonth; i++) {
+        if (i == new Date().getDate() && D.getFullYear() == new Date().getFullYear() && D.getMonth() == new Date().getMonth()) {
+            let day = document.createElement('div');
+            day.classList.add('day');
+            calendar.append(day);
+            day.innerText = i;
+            day.classList.add('today');
+        } else {
+            let day = document.createElement('div');
+            day.classList.add('day');
+            calendar.append(day);
+            day.innerText = i;
+        }
+    }
+    
+    if(dayOfWeekLastDayOfMonth !=0) {
+        for(let i=dayOfWeekLastDayOfMonth; i<7; i++) {
+            let day = document.createElement('div');
+            day.classList.add('day');
+            calendar.append(day);  
+        }
+    }
+     
+    currentMonth.innerHTML = D.getFullYear() + ` ` + monthArray[D.getMonth()]
+    
+    for (let i=0; i<publicHolidays.length; i++) {
+        if(monthArray[D.getMonth()]  === publicHolidays[i].month) {
+            let items = calendar.querySelectorAll('.day');
+            for (let item of items) {
+                if(item.innerHTML === publicHolidays[i].day) {
+                    item.classList.add('publicHoliday')
+                }
+            }
+        }
+    }
+}
+
+makeCalendar(getCurrentYear, getCurrentMonth);
+
+prevButton.addEventListener('click', function() {
+    calendar.innerHTML = '';
+    let month;
+    let year;
+    if(getCurrentMonth > 0) {
+        getCurrentMonth = getCurrentMonth - 1;
+        month = getCurrentMonth;
+    } else {
+        getCurrentMonth = 11;
+        month = 11;
+    }
+    
+    if (getCurrentMonth === 11) {
+        getCurrentYear = getCurrentYear - 1;
+        year = getCurrentYear;
+    } else {
+        year = getCurrentYear
+    }
+   
+ makeCalendar(year, month);
+    
+})
+
+nextButton.addEventListener('click', function() {
+    calendar.innerHTML = '';
+    let month;
+    
+    let year;
+
+    if (getCurrentMonth !== 11) {
+        getCurrentMonth = getCurrentMonth + 1;
+        month = getCurrentMonth;
+    } else {
+        getCurrentMonth = 0;
+        month = 0;
+    }
+    if (getCurrentMonth === 0) {
+        getCurrentYear = getCurrentYear + 1;
+        year = getCurrentYear;
+       
+    } else {
+        year = getCurrentYear ;
+    }
+ makeCalendar(year, month);
+    
+})
--- projects/goodWeather/js/index.js
+++ projects/goodWeather/js/index.js
@@ -17,102 +17,119 @@ let forecast = document.getElementById("forecast");
 forecast.style = `
   display: flex;
   justify-content: space-around;
+  border-top: 1px solid white;
+  margin-top: 40px;
 `;
 
 let cityText = "Глубокое";
 let units = "imperial";
 
 fahrenheit.onclick = () => {
-  fahrenheit.classList.add("active");
-  celsius.classList.remove("active");
+    fahrenheit.classList.add("active");
+    celsius.classList.remove("active");
 
-  units = "imperial";
-  WeatherService.getWeatherByText(cityText, units).then((result) =>
-    renderWeatherData(result)
-  );
+    units = "imperial";
+    WeatherService.getWeatherByText(cityText, units).then((result) =>
+        renderWeatherData(result)
+    );
 };
 
 celsius.onclick = () => {
-  celsius.classList.add("active");
-  fahrenheit.classList.remove("active");
+    celsius.classList.add("active");
+    fahrenheit.classList.remove("active");
 
-  units = "metric";
-  WeatherService.getWeatherByText(cityText, units).then((result) =>
-    renderWeatherData(result)
-  );
+    units = "metric";
+    WeatherService.getWeatherByText(cityText, units).then((result) =>
+        renderWeatherData(result)
+    );
 };
 
-form.onsubmit = function (event) {
-  event.preventDefault();
-  let input = event.target[0];
+form.onsubmit = function(event) {
+    event.preventDefault();
+    let input = event.target[0];
 
-  WeatherService.getWeatherByText(input.value, units)
-    .then((result) => renderWeatherData(result))
-    .catch(() => {
-      input.value = "";
+    WeatherService.getWeatherByText(input.value, units)
+        .then((result) => {
+            renderWeatherData(result);
+            cityText = input.value
+        })
+        .catch(() => {
+            input.value = "";
 
-      alert("Не вводи такой ерунды больше никогда!");
-    });
+            alert("Не вводи такой ерунды больше никогда!");
+        });
 };
 
 WeatherService.getWeatherByText(cityText).then((result) =>
-  renderWeatherData(result)
+    renderWeatherData(result)
 );
 
 function renderWeatherData(weatherData) {
-  if (weatherData?.name) {
-    panel.classList.remove("transparent");
-    preloader.classList.remove("active");
-  }
+    if (weatherData?.name) {
+        panel.classList.remove("transparent");
+        preloader.classList.remove("active");
+    }
 
-  temp.innerHTML = weatherData.main.temp;
-  city.innerHTML = weatherData.name;
-  description.innerHTML = weatherData.weather[0].description;
+    temp.innerHTML = weatherData.main.temp;
+    city.innerHTML = weatherData.name;
+    description.innerHTML = weatherData.weather[0].description;
 
-  let windText = `${weatherData.wind.speed} ${
+    let windText = `${weatherData.wind.speed} ${
     units === "metric" ? "km/h" : "mph"
   }`;
 
-  wind.innerHTML = windText;
+    wind.innerHTML = windText;
 
-  let humidityText = `${weatherData.main.humidity}%`;
+    let humidityText = `${weatherData.main.humidity}%`;
 
-  humidity.innerHTML = humidityText;
-  icon.src = `http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`;
+    humidity.innerHTML = humidityText;
+    icon.src = `http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`;
 
-  showDate();
+    showDate();
 
-  WeatherService.getForecast(weatherData.name, units).then((result) => {
-    let { list } = result;
+    WeatherService.getForecast(weatherData.name, units).then((result) => {
+        let { list } = result;
 
-    let elemArr = list.map((item) => {
-      return ForecastItem({ maxTemp: item.temp.max, minTemp: item.temp.min });
+        let elemArr = list.map((item) => {
+            return ForecastItem({ 
+                maxTemp: item.temp.max, 
+                minTemp: item.temp.min, 
+                dateOfForecast: item.dt, 
+                sunriseTime: item.sunrise, 
+                sunsetTime: item.sunset});
+        });
+        console.log(result)
+        forecast.innerHTML = "";
+        forecast.append(...elemArr);
     });
-
-    forecast.innerHTML = "";
-    forecast.append(...elemArr);
-  });
 }
 
 function showDate() {
-  let date = new Date();
+    let date = new Date();
 
-  dt.innerHTML = date.toLocaleDateString();
+    dt.innerHTML = date.toLocaleDateString();
 }
 
-function ForecastItem({ maxTemp, minTemp }) {
-  let div = document.createElement("div");
+function ForecastItem({ maxTemp, minTemp, dateOfForecast, sunriseTime, sunsetTime}) {
+    let div = document.createElement("div");
 
-  div.style = `
+    div.style = `
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
-    margin-top: 33px;
+    margin-top: 10px;
+    color: white;
   `;
 
-  div.innerHTML = `<h1>${maxTemp}</h1>  
-  <h2>${minTemp}</h2>`;
+  let forecastDate = new Date(dateOfForecast * 1000).toLocaleDateString().substr(0, 5);
+  
+  div.innerHTML = `<p style="font-size: 20px; opacity: 0.9">${forecastDate}</p> 
+    <span style="opacity: 50%; font-size: 15px; text-transform: lowercase">Max</span> <h1 style="margin-bottom:5px">${maxTemp}</h1> 
+    <span style="opacity: 50%; font-size: 15px; text-transform: lowercase">min</span> <h2 style="margin-bottom:10px">${minTemp}</h2> 
+    <h3 style="text-transform: capitalize; font-size: 12px; opacity: 0.6">Sunrise: ${new Date(sunriseTime * 1000).toLocaleTimeString().substr(0, 5)}</h3> 
+    <h3 style="text-transform: capitalize; font-size: 12px; opacity: 0.6">Sunset: ${new Date(sunsetTime * 1000).toLocaleTimeString().substr(0, 5)}</h3>
+    `;
 
-  return div;
+    return div;
 }
